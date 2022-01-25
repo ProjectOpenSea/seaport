13c61c4cb4645e08751c775ce68e1c9ef720805d
Vicky Gong <vicky.gong@opensea.io>
2022-01-25 16:31:37 -0500

    pushing what we have

--- test/index.ts
+++ test/index.ts
@@ -1,11 +1,16 @@
+import { TypedDataDomain } from "@ethersproject/abstract-signer";
 import { JsonRpcProvider } from "@ethersproject/providers";
 import { Wallet } from "@ethersproject/wallet";
 import { expect } from "chai";
 import { time } from "console";
 import { constants } from "ethers";
 import { ethers } from "hardhat";
+import { TypedData, TypedDataUtils } from "ethers-eip712";
 import { Consideration, TestERC721 } from "../typechain-types";
-import { OrderParametersStruct } from "../typechain-types/Consideration";
+import {
+  OrderComponentsStruct,
+  OrderParametersStruct,
+} from "../typechain-types/Consideration";
 import { faucet, whileImpersonating } from "./utils/impersonate";
 
 describe("Consideration functional tests", function () {
@@ -13,64 +18,130 @@ describe("Consideration functional tests", function () {
   let chainId: number;
   let marketplaceContract: Consideration;
   let testERC721: TestERC721;
+  let owner: Wallet;
+  let domainData: TypedData["domain"];
+
+  const considerationTypesEip712Hash = {
+    OrderComponents: [
+      {
+        name: "offerer",
+        type: "address",
+      },
+      { name: "facilitator", type: "address" },
+      { name: "orderType", type: "uint8" },
+      { name: "startTime", type: "uint256" },
+      { name: "endTime", type: "uint256" },
+      { name: "salt", type: "uint256" },
+      { name: "offer", type: "Asset[]" },
+      { name: "consideration", type: "ReceivedAsset[]" },
+      { name: "nonce", type: "uint256" },
+    ],
+    Asset: [
+      { name: "assetType", type: "uint8" },
+      { name: "token", type: "address" },
+      { name: "identifierOrCriteria", type: "uint256" },
+      { name: "amount", type: "uint256" },
+    ],
+    ReceivedAsset: [
+      { name: "assetType", type: "uint8" },
+      { name: "token", type: "address" },
+      { name: "identifierOrCriteria", type: "uint256" },
+      { name: "amount", type: "uint256" },
+      { name: "account", type: "address" },
+    ],
+  };
+
+  before(async () => {
+    const network = await provider.getNetwork();
+    chainId = network.chainId;
+    owner = ethers.Wallet.createRandom().connect(provider);
+    await Promise.all(
+      [owner].map((wallet) => faucet(wallet.address, provider))
+    );
+
+    const considerationFactory = await ethers.getContractFactory(
+      "Consideration"
+    );
+    const TestERC721Factory = await ethers.getContractFactory(
+      "TestERC721",
+      owner
+    );
+    marketplaceContract = await considerationFactory.deploy();
+    testERC721 = await TestERC721Factory.deploy();
+
+    // Required for EIP712 signing
+    domainData = {
+      name: "Consideration",
+      version: "1",
+      chainId: chainId,
+      verifyingContract: marketplaceContract.address,
+    };
+  });
 
   // Buy now or accept offer for a single ERC721 or ERC1155 in exchange for
   // ETH, WETH or ERC20
   describe("Basic buy now or accept offer flows", async () => {
-    let owner: Wallet;
     let seller: Wallet;
     let buyer: Wallet;
 
     beforeEach(async () => {
-      const network = await provider.getNetwork();
-      chainId = network.chainId;
       // Setup basic buyer/seller wallets with ETH
-      owner = ethers.Wallet.createRandom().connect(provider);
       seller = ethers.Wallet.createRandom().connect(provider);
       buyer = ethers.Wallet.createRandom().connect(provider);
       await Promise.all(
         [owner, seller, buyer].map((wallet) => faucet(wallet.address, provider))
       );
-      const considerationFactory = await ethers.getContractFactory(
-        "Consideration"
-      );
-      const TestERC721Factory = await ethers.getContractFactory(
-        "TestERC721",
-        owner
-      );
-      marketplaceContract = await considerationFactory.deploy();
-      testERC721 = await TestERC721Factory.deploy();
     });
 
-    const considerationOrderTypesHash = {
-      OrderComponents: [
-        {
-          name: "offerer",
-          type: "address",
+    // Returns signature
+    async function signOrder(
+      orderComponents: OrderComponentsStruct,
+      signer: Wallet
+    ) {
+      return await signer._signTypedData(
+        domainData,
+        considerationTypesEip712Hash,
+        orderComponents
+      );
+    }
+
+    async function signOrderWithEip712Lib(
+      orderComponents: OrderComponentsStruct,
+      signer: Wallet
+    ) {
+      const typedData: TypedData = {
+        types: {
+          EIP712Domain: [
+            { name: "name", type: "string" },
+            { name: "version", type: "string" },
+            { name: "chainId", type: "uint256" },
+            { name: "verifyingContract", type: "address" },
+          ],
+          ...considerationTypesEip712Hash,
         },
-        { name: "facilitator", type: "address" },
-        { name: "orderType", type: "uint8" },
-        { name: "startTime", type: "uint256" },
-        { name: "endTime", type: "uint256" },
-        { name: "salt", type: "uint256" },
-        { name: "offer", type: "Asset[]" },
-        { name: "consideration", type: "ReceivedAsset[]" },
-        { name: "nonce", type: "uint256" },
-      ],
-      Asset: [
-        { name: "assetType", type: "uint8" },
-        { name: "token", type: "address" },
-        { name: "identifierOrCriteria", type: "uint256" },
-        { name: "amount", type: "uint256" },
-      ],
-      ReceivedAsset: [
-        { name: "assetType", type: "uint8" },
-        { name: "token", type: "address" },
-        { name: "identifierOrCriteria", type: "uint256" },
-        { name: "amount", type: "uint256" },
-        { name: "account", type: "address" },
-      ],
-    };
+        primaryType: "OrderComponents" as const,
+        domain: domainData,
+        message: orderComponents,
+      };
+
+      const digest = TypedDataUtils.encodeDigest(typedData);
+      const digestHex = ethers.utils.hexlify(digest);
+      console.log("digest: ", digest);
+      console.log("DigestHex: ", digestHex);
+
+      // const encodedData = TypedDataUtils.encodeData(typedData);
+      return await signer.signMessage(ethers.utils.arrayify(digest));
+      /**
+       * digest:  Uint8Array(32) [
+  144,  8, 170,  95,  35, 140,  11, 225,
+  155, 39, 211,  59, 103,  31, 118, 103,
+  152, 30, 114, 187, 113, 200,  32, 248,
+  181, 51, 116, 137, 120,  45, 189, 158
+]
+DigestHex:  0x9008aa5f238c0be19b27d33b671f7667981e72bb71c820f8b5337489782dbd9e
+sigFromEip712Lib: 0x44f6c0e7d88f980f29da33b3e3ecbef759fbbe80e6b9e94f5b91af589696f20a173e93f5028dcc07c3b4b789e7099ef37013a83dfbd8061d1065115a3bc74e481b
+       */
+    }
 
     describe("A single ERC721 is to be transferred", async () => {
       describe("[Buy now] User fullfills a sell order for a single ERC721", async () => {
@@ -108,31 +179,35 @@ describe("Consideration functional tests", function () {
             ],
           };
 
-          const domainData = {
-            name: "Consideration",
-            version: "1",
-            chainId,
-            verifyingContract: marketplaceContract.address,
-          };
-          const orderComponent = {
+          const orderComponents = {
             ...orderParameters,
             nonce: 0,
           };
-          console.log("Ordercomponent:", orderComponent);
-          const flatSig = await seller._signTypedData(
-            domainData,
-            considerationOrderTypesHash,
-            ethers.utils.arrayify(orderComponent)
-          );
+
+          const flatSig = await signOrder(orderComponents, seller);
           console.log("flatsig:", flatSig);
+
+          const sigFromEip712Lib = await signOrderWithEip712Lib(
+            orderComponents,
+            seller
+          );
+          console.log("sigFromEip712Lib:", sigFromEip712Lib);
+
           const order = {
             parameters: orderParameters,
-            signature: flatSig,
+            signature: sigFromEip712Lib,
           };
           const orderHash = await marketplaceContract
             .connect(buyer.address)
-            .getOrderHash(orderComponent);
+            .getOrderHash(orderComponents);
           console.log("orderHash", orderHash);
+
+          const domainSeparator = await marketplaceContract
+            .connect(buyer.address)
+            .DOMAIN_SEPARATOR();
+          console.log("domainSeparator", domainSeparator);
+
+          // recover signer from signature and domain separator + order hash
 
           await whileImpersonating(buyer.address, provider, async () => {
             await expect(marketplaceContract.connect(buyer).fulfillOrder(order))
