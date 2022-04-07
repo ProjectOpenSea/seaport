476cc936b33423b86b42b2e2a3d23ba0b4aabd15
Joseph Schiarizzi <9449596+jschiarizzi@users.noreply.github.com>
2022-04-06 21:42:14 -0400

    outline edges

--- src/test/Consideration.t.sol
+++ src/test/Consideration.t.sol
@@ -22,6 +22,8 @@ contract ConsiderationTest is DSTest {
 
       vm = VM(HEVM_ADDRESS);
 
+      consider = new Consideration(address(0), address(0));
+
       //deploy a test 721
       test721 = new NFT("Nifty", "NFT");
 
@@ -32,37 +34,116 @@ contract ConsiderationTest is DSTest {
       accountB = 0xb8722FD62E5589241228970b165aB617ed186AeD;
       accountC = 0x81f464ed27111E8c1606546D5DC7fD72fF45EE0e;
 
+      for(uint i; i < 50; i++){
+        test721.mintTo(accountA);
+      }
+
       vm.label(accountA, "Account A");
 
     }
 
-    function testCreateBasicOrderA() public {
+    //basic orders
+    function testBasicOrder721toEth() external {
+        emit log("Basic Orders, Buy Now");
         address seller = accountA;
-        assertTrue(true, "this is true.");
-
         vm.startPrank(seller);
     }
 
-    function testFullfilOrders() public {
-        emit log("this will fail...");
-
-        assertTrue(true, "this is false.");
+    function testBasicOrder721to20() external {
+        address seller = accountA;
+        vm.startPrank(seller);
     }
 
-    function testCreateDescendingPriceOrders() public {
-        emit log("this will fail...");
+    function testBasicOrder1155toEth() external {
+        address seller = accountA;
+        vm.startPrank(seller);
+    }
+
+    function testBasicOrder1155to20() external {
+        address seller = accountA;
+        vm.startPrank(seller);
+    }
+
+    //match
+    function testMatchOrder721toEth() external {
+        emit log("Basic Order, Match Order")
+        address seller = accountA;
+        vm.startPrank(seller);
+    }
+
+    function testMatchOrder721to20() external {
+        address seller = accountA;
+        vm.startPrank(seller);
+    }
+
+    function testMatchOrder1155toEth() external {
+        address seller = accountA;
+        vm.startPrank(seller);
+    }
+
+    function testMatchOrder1155to20() external {
+        address seller = accountA;
+        vm.startPrank(seller);
+    }
+
+    //getters
+    function testGetters() external{
+      emit log("Getter Tests");
+      address seller = accountA;
+      vm.startPrank(seller);
+    }
+
+    function testCanceledOrder() external{
+      emit log("Cancel Order");
+      address seller = accountA;
+      vm.startPrank(seller);
+    }
+
+    function TestSequenceOrder(){
+      emit log("SequenceOrder");
+      address seller = accountA;
+      vm.startPrank(seller);
+    }
+
+    function testFailNoSequencerder(){
+      address seller = accountA;
+      vm.startPrank(seller);
+    }
+
+    function testFailReenterSell(){
+      emit log("Re-entrency tests on ever function that should have it.");
+      address seller = accountA;
+      vm.startPrank(seller);
+    }
+
+    function testFailReenterSell(){
+      emit log("Re-entrency tests on ever function that should have it.");
+      address seller = accountA;
+      address buyer = accountB;
+
+      //make a valid order
+      vm.prank(seller);
+
+      //make a valid buy but try to re-enter using a callback.
+      //TODO this will need a second helper contract to have a callback since our test tokens do not have it.
+    }
+
+    function testFailInsufficientBuys() external{
+      emit log("Insufficient amounts and bad items orders.")
+    }
+
+    //ascending and descending
+    function testCreateDescendingPriceOrders() external {
+        emit log("TODO");
 
         assertTrue(false, "this is false.");
     }
 
-    function testCreateAscendingPriceOrders() public {
-        emit log("this will fail...");
+    function testCreateAscendingPriceOrders() external {
+        emit log("TODO");
 
         assertTrue(false, "this is false.");
     }
 
-    function testFillOrders() public {
 
-      assertTrue(true, "this is true");
-    }
 }
