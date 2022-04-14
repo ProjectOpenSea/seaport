64abec6c5ca35a609738d03e7c37b0eebff9d035
Joseph Schiarizzi <9449596+jschiarizzi@users.noreply.github.com>
2022-04-14 14:55:09 -0400

    basic order signing fix

--- src/test/Consideration.t.sol
+++ src/test/Consideration.t.sol
@@ -4,6 +4,8 @@
 pragma solidity 0.8.12;
 
 import "ds-test/test.sol";
+import { OrderType, BasicOrderType, ItemType, Side } from "../../contracts/lib/ConsiderationEnums.sol";
+import { AdditionalRecipient } from "../../contracts/lib/ConsiderationStructs.sol";
 import "../../contracts/Consideration.sol";
 import "src/test/NFT721.sol";
 import "src/test/CheatCodes.sol";
@@ -26,8 +28,11 @@ contract ConsiderationTest is DSTest {
     address test721Address;
     NFT test721;
 
+    address zone;
+
     function setUp() public {
         vm = CheatCodes(HEVM_ADDRESS);
+        zone = address(0);
 
         considerAddress = address(new Consideration(address(0), address(0)));
         consider = Consideration(consider);
@@ -36,12 +41,9 @@ contract ConsiderationTest is DSTest {
         test721Address = address(new NFT("Nifty", "NFT"));
         test721 = NFT(test721Address);
 
-        //d6e900755be565cb8eb4dbd2bbb77583c5996f9c254aa80c6270d8756f6efb00
-        //d6e900755be565cb8eb4dbd2bbb77583c5996f9c254aa80c6270d8756f6efb01
-        //d6e900755be565cb8eb4dbd2bbb77583c5996f9c254aa80c6270d8756f6efb02
-        accountA = 0xEbe047B1229E95E6a4F03039eF17140Bd6E2A1F0;
-        accountB = 0xb8722FD62E5589241228970b165aB617ed186AeD;
-        accountC = 0x81f464ed27111E8c1606546D5DC7fD72fF45EE0e;
+        accountA = vm.addr(1);
+        accountB = vm.addr(2);
+        accountC = vm.addr(3);
 
         for (uint256 i; i < 10; i++) {
             test721.mintTo(accountA);
@@ -62,19 +64,62 @@ contract ConsiderationTest is DSTest {
     //basic Order
 
     //eth to 721
+    //accountA is offering their 721 for ETH
     function testListBasicETHto721(
         uint256 _id,
         uint256 _ethAmount,
         bytes32 _zoneHash,
-        uint256 _salt,
-        bytes sig
+        uint256 _salt
     ) external {
-        vm.cheats.assume(_id > 0);
-        vm.cheats.assume(_id < 10);
+        vm.assume(_id > 0);
+        vm.assume(_id < 10);
         emit log("Basic Order");
 
-        address alice vm.cheat.addr(1);
-        (uint8 v, bytes32 r, bytes32 s) = vm.cheats.sign(1, _zoneHash);
+        OfferItem memory offerItem = new OfferItem(
+            ItemType.ERC721,
+            test721Address,
+            _id,
+            1,
+            1
+        );
+        OfferItem[] memory offer = OfferItem[](1);
+        offer[0] = offerItem;
+
+        ConsiderationItem memory considerationItem = new ConsiderationItem(
+            ItemType.NATIVE,
+            address(0),
+            0,
+            _ethAmount,
+            _ethAmount,
+            accountA
+        );
+        ConsiderationItem[] memory consideration = ConsiderationItem[](1);
+        consideration[0] = considerationItem;
+
+        uint256 nonce = consider.getNonce(accountA);
+        //getOrderHash
+        OrderComponents memory orderComponents = new OrderComponents(
+            accountA,
+            zone,
+            offer,
+            consideration,
+            0,
+            block.timestamp,
+            block.timestamp + 5000,
+            _zoneHash,
+            _salt,
+            nonce
+        );
+        bytes32 orderHash = consider.getOrderHash(OrderComponents);
+        bytes32 domainSep = consider.DOMAIN_SEPARATOR();
+
+        bytes32 digest = keccak256(
+            abi.encodePacked(0x1901, domainSep, orderHash)
+        );
+
+        //accountA is pk 1.
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(1, digest);
+        bytes memory sig = abi.encodePacked(r, s, v);
 
         address considerationToken = address(0); // eth
         uint256 considerationIdentifier = 0; //TODO check on this
@@ -82,22 +127,23 @@ contract ConsiderationTest is DSTest {
         address payable offerer = accountA;
         address zone = accountB;
         address offerToken = test721Address;
-        uint256 id = _id;
+        uint256 offerIdentifier = _id;
         uint256 offerAmount = 1;
-        BasicOrderType basicOrderType = 0; // eth to 721 open
+        BasicOrderType basicOrderType = BasicOrderType.ETH_TO_ERC721_FULL_OPEN; // eth to 721 open
         uint256 startTime = block.timestamp; // 0x144
         uint256 endTime = block.timestamp + 5000; // 0x164
         bytes32 zoneHash = _zoneHash; // 0x184
         uint256 salt = _salt; // 0x1a4
         bool useFulfillerProxy = false; // 0x1c4
         uint256 totalOriginalAdditionalRecipients = 0; // 0x1e4
-        AdditionalRecipient[] additionalRecipients = []; // 0x204
-        bytes signature = sig; // 0x224
+        AdditionalRecipient[]
+            memory additionalRecipients = new AdditionalRecipient[](0); // 0x204
+        bytes memory signature = sig; // 0x224
         // Total length, excluding dynamic array data: 0x244 (580)
 
         //list
         vm.prank(accountA);
-        BasicOrderParameters order = new BasicOrderParameters(
+        BasicOrderParameters memory order = new BasicOrderParameters(
             considerationToken,
             considerationIdentifier,
             considerationAmount,
