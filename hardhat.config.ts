00be5b435058e39b4e2284aa051eb91a5700f06c
Sam Barnes <mistersamuelbarnes@gmail.com>
2022-04-14 17:05:12 -0600

    bump hardhat to 0.8.13 to match contracts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -27,7 +27,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.12",
+        version: "0.8.13",
         settings: {
           optimizer: {
             enabled: true,
