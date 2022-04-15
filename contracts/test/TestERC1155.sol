08cca616055d0749242a43c46bb11a5a0052f87b
0age <0age@protonmail.com>
2022-04-15 00:30:30 -0700

    oh right, the linter

--- contracts/test/TestERC1155.sol
+++ contracts/test/TestERC1155.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.13;
 import "@rari-capital/solmate/src/tokens/ERC1155.sol";
 
 // Used for minting test ERC1155s in our tests
-contract TestERC1155 is ERC1155() {
+contract TestERC1155 is ERC1155 {
     function mint(
         address to,
         uint256 tokenId,
