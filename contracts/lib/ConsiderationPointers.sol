7c23a7dc6fc5a9aebd110916358840d087e46c4e
d1ll0n <dillonkellar@protonmail.com>
2022-04-19 23:39:35 -0500

    Add prelude with disambiguation and notes

--- contracts/lib/ConsiderationPointers.sol
+++ contracts/lib/ConsiderationPointers.sol
@@ -1,6 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
+/*
+ * -------------------------- Disambiguation & Other Notes --------------------------
+ *    - The term "head" is used as it is in the documentation for ABI encoding,
+ *      but only in reference to dynamic types, i.e. it always refers to the offset
+ *      or pointer to the body of a dynamic type. In calldata, the head is always
+ *      an offset (relative to the parent object), while in memory, the head is always
+ *      the pointer to the body 
+ *        
+ *        - https://docs.soliditylang.org/en/v0.8.13/abi-spec.html#argument-encoding
+ *        - Note that the length of an array is separate from and precedes the head of
+ *          the array.
+ *
+ *    - The term "body" is used in place of the term "head" used in the ABI documentation.
+ *      It refers to the start of the data for a dynamic type, e.g. the first word of a
+ *       struct or the first word of the first element in an array.
+ *
+ *    - "pointer" is used to describe the absolute position of a value and never an offset
+ *      relative to another value.
+ *
+ *        - The suffix "_ptr" refers to a memory pointer.
+ *        - The suffix "_cdPtr" refers to a calldata pointer.
+ *
+ *    - "offset" is used to describe the position of a value relative to some parent value.
+ *      For example, OrderParameters_conduit_offset is the offset to the "conduit" value in the
+ *      OrderParameters struct relative to the start of the body.
+ *
+ *        - Note: Offsets are used to derive pointers.
+ *
+ *    - Some structs have pointers defined for all of their fields in this file.
+ *      Lines which are commented out are fields that are not used in the codebase but
+ *      have been left in for readability.
+ */
+
 // Common Offsets
 // Offsets for identically positioned fields shared by:
 // OfferItem, ConsiderationItem, SpentItem, ReceivedItem
