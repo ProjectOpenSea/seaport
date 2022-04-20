1595af7906e9dc25fa8f1659f6f034d158caf440
d1ll0n <dillonkellar@protonmail.com>
2022-04-19 23:39:16 -0500

    Rename Order_x_offset -> OrderParameters_x_offset & CommonXOffset -> Common_x_offset. Comment out unused pointers

--- contracts/lib/ConsiderationPointers.sol
+++ contracts/lib/ConsiderationPointers.sol
@@ -2,21 +2,25 @@
 pragma solidity 0.8.13;
 
 // Common Offsets
-// Offsets to fields within -Item structs
+// Offsets for identically positioned fields shared by:
+// OfferItem, ConsiderationItem, SpentItem, ReceivedItem
 
-// uint256 constant CommonItemTypeOffset = 0x0;
-uint256 constant CommonTokenOffset = 0x20;
-uint256 constant CommonIdentifierOffset = 0x40;
-uint256 constant CommonAmountOffset = 0x60;
+uint256 constant Common_token_offset = 0x20;
+uint256 constant Common_identifier_offset = 0x40;
+uint256 constant Common_amount_offset = 0x60;
 
+uint256 constant ReceivedItem_size = 0xa0;
+uint256 constant ReceivedItem_amount_offset = 0x60;
 uint256 constant ReceivedItem_recipient_offset = 0x80;
+
 uint256 constant ConsiderationItem_recipient_offset = 0xa0;
+
 uint256 constant Execution_offerer_offset = 0x20;
 uint256 constant Execution_conduit_offset = 0x40;
 
-uint256 constant Order_offer_head_offset = 0x40;
-uint256 constant Order_consideration_head_offset = 0x60;
-uint256 constant Order_conduit_offset = 0x120;
+uint256 constant OrderParameters_offer_head_offset = 0x40;
+uint256 constant OrderParameters_consideration_head_offset = 0x60;
+uint256 constant OrderParameters_conduit_offset = 0x120;
 
 uint256 constant Fulfillment_itemIndex_offset = 0x20;
 
@@ -36,11 +40,9 @@ uint256 constant EIP712_Order_size = 0x180;
 uint256 constant EIP712_OfferItem_size = 0xc0;
 uint256 constant EIP712_ConsiderationItem_size = 0xe0;
 uint256 constant AdditionalRecipients_size = 0x40;
-uint256 constant ReceivedItem_size = 0xa0;
 
 uint256 constant receivedItemsHash_ptr = 0x60;
 
-uint256 constant ReceivedItem_amount_offset = 0x60;
 
 /*
  *  Memory layout in _prepareBasicFulfillmentFromCalldata of
@@ -74,14 +76,20 @@ uint256 constant ReceivedItem_amount_offset = 0x60;
  *  - 0x1c0: considerationRecipient
  *  - ...
  */
+
+
+// Minimum length of the OrderFulfilled event data.
+// Must be added to the size of the ReceivedItem array for additionalRecipients
+// (0xa0 * additionalRecipients.length) to calculate the full size of the buffer.
 uint256 constant OrderFulfilled_baseSize = 0x1e0;
-// Offset in memory to OrderFulfilled before adding the size
-// of the received items for additionalRecipients
+// Minimum offset in memory to OrderFulfilled event data.
+// Must be added to the size of the EIP712 hash array for additionalRecipients
+// (32 * additionalRecipients.length) to calculate the pointer to the event data.
 uint256 constant OrderFulfilled_baseOffset = 0x180;
 uint256 constant OrderFulfilled_consideration_length_baseOffset = 0x2a0;
 uint256 constant OrderFulfilled_offer_length_baseOffset = 0x200;
 
-uint256 constant OrderFulfilled_orderHash_offset = 0x00;
+// uint256 constant OrderFulfilled_orderHash_offset = 0x00;
 uint256 constant OrderFulfilled_fulfiller_offset = 0x20;
 uint256 constant OrderFulfilled_offer_head_offset = 0x40;
 uint256 constant OrderFulfilled_offer_body_offset = 0x80;
@@ -90,23 +98,23 @@ uint256 constant OrderFulfilled_consideration_body_offset = 0x120;
 
 // BasicOrderParameters
 uint256 constant BasicOrder_considerationToken_cdPtr = 0x24;
-uint256 constant BasicOrder_considerationIdentifier_cdPtr = 0x44;
+// uint256 constant BasicOrder_considerationIdentifier_cdPtr = 0x44;
 uint256 constant BasicOrder_considerationAmount_cdPtr = 0x64;
 uint256 constant BasicOrder_offerer_cdPtr = 0x84;
 uint256 constant BasicOrder_zone_cdPtr = 0xa4;
 uint256 constant BasicOrder_offerToken_cdPtr = 0xc4;
-uint256 constant BasicOrder_offerIdentifier_cdPtr = 0xe4;
+// uint256 constant BasicOrder_offerIdentifier_cdPtr = 0xe4;
 uint256 constant BasicOrder_offerAmount_cdPtr = 0x104;
-uint256 constant BasicOrder_basicOrderType_cdPtr = 0x124;
+// uint256 constant BasicOrder_basicOrderType_cdPtr = 0x124;
 uint256 constant BasicOrder_startTime_cdPtr = 0x144;
-uint256 constant BasicOrder_endTime_cdPtr = 0x164;
-uint256 constant BasicOrder_zoneHash_cdPtr = 0x184;
-uint256 constant BasicOrder_salt_cdPtr = 0x1a4;
-uint256 constant BasicOrder_offererConduit_cdPtr = 0x1c4;
-uint256 constant BasicOrder_fulfillerConduit_cdPtr = 0x1e4;
+// uint256 constant BasicOrder_endTime_cdPtr = 0x164;
+// uint256 constant BasicOrder_zoneHash_cdPtr = 0x184;
+// uint256 constant BasicOrder_salt_cdPtr = 0x1a4;
+// uint256 constant BasicOrder_offererConduit_cdPtr = 0x1c4;
+// uint256 constant BasicOrder_fulfillerConduit_cdPtr = 0x1e4;
 uint256 constant BasicOrder_totalOriginalAdditionalRecipients_cdPtr = 0x204;
-uint256 constant BasicOrder_additionalRecipients_cdPtr = 0x224;
-uint256 constant BasicOrder_signature_cdPtr = 0x244;
+// uint256 constant BasicOrder_additionalRecipients_head_cdPtr = 0x224;
+// uint256 constant BasicOrder_signature_cdPtr = 0x244;
 uint256 constant BasicOrder_additionalRecipients_length_cdPtr = 0x264;
 uint256 constant BasicOrder_additionalRecipients_data_cdPtr = 0x284;
 
@@ -127,7 +135,7 @@ uint256 constant BasicOrder_considerationItem_token_ptr = 0xc0;
 uint256 constant BasicOrder_considerationItem_identifier_ptr = 0xe0;
 uint256 constant BasicOrder_considerationItem_startAmount_ptr = 0x100;
 uint256 constant BasicOrder_considerationItem_endAmount_ptr = 0x120;
-uint256 constant BasicOrder_considerationItem_recipient_ptr = 0x140;
+// uint256 constant BasicOrder_considerationItem_recipient_ptr = 0x140;
 
 /*
  *  Memory layout in _prepareBasicFulfillmentFromCalldata of
@@ -142,8 +150,8 @@ uint256 constant BasicOrder_considerationItem_recipient_ptr = 0x140;
 uint256 constant BasicOrder_offerItem_typeHash_ptr = DefaultFreeMemoryPointer;
 uint256 constant BasicOrder_offerItem_itemType_ptr = 0xa0;
 uint256 constant BasicOrder_offerItem_token_ptr = 0xc0;
-uint256 constant BasicOrder_offerItem_identifier_ptr = 0xe0;
-uint256 constant BasicOrder_offerItem_startAmount_ptr = 0x100;
+// uint256 constant BasicOrder_offerItem_identifier_ptr = 0xe0;
+// uint256 constant BasicOrder_offerItem_startAmount_ptr = 0x100;
 uint256 constant BasicOrder_offerItem_endAmount_ptr = 0x120;
 
 /*
@@ -164,13 +172,13 @@ uint256 constant BasicOrder_offerItem_endAmount_ptr = 0x120;
  */
 uint256 constant BasicOrder_order_typeHash_ptr = 0x80;
 uint256 constant BasicOrder_order_offerer_ptr = 0xa0;
-uint256 constant BasicOrder_order_zone_ptr = 0xc0;
+// uint256 constant BasicOrder_order_zone_ptr = 0xc0;
 uint256 constant BasicOrder_order_offerHashes_ptr = 0xe0;
 uint256 constant BasicOrder_order_considerationHashes_ptr = 0x100;
 uint256 constant BasicOrder_order_orderType_ptr = 0x120;
 uint256 constant BasicOrder_order_startTime_ptr = 0x140;
-uint256 constant BasicOrder_order_endTime_ptr = 0x160;
-uint256 constant BasicOrder_order_zoneHash_ptr = 0x180;
-uint256 constant BasicOrder_order_salt_ptr = 0x1a0;
-uint256 constant BasicOrder_order_conduit_ptr = 0x1c0;
+// uint256 constant BasicOrder_order_endTime_ptr = 0x160;
+// uint256 constant BasicOrder_order_zoneHash_ptr = 0x180;
+// uint256 constant BasicOrder_order_salt_ptr = 0x1a0;
+// uint256 constant BasicOrder_order_conduit_ptr = 0x1c0;
 uint256 constant BasicOrder_order_nonce_ptr = 0x1e0;
