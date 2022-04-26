4d7d192723c8f73e35aebd266345fd9df220b5a6
James Wenzel <wenzel.james.r@gmail.com>
2022-04-26 13:05:03 -0700

    implement joseph's new tests in consideration.t.sol add sign helper method to baseconsiderationtest add tokenrecipient superclasses

--- test/foundry/utils/BaseConsiderationTest.sol
+++ test/foundry/utils/BaseConsiderationTest.sol
@@ -1,93 +1,50 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity 0.8.13;
-
-import "../../../contracts/conduit/ConduitController.sol";
-
-import { Consideration } from "../../../contracts/Consideration.sol";
-import { OrderType, BasicOrderType, ItemType, Side } from "../../../contracts/lib/ConsiderationEnums.sol";
-import { OfferItem, ConsiderationItem, OrderComponents, BasicOrderParameters } from "../../../contracts/lib/ConsiderationStructs.sol";
 
 import { DSTestPlusPlus } from "./DSTestPlusPlus.sol";
 import { stdStorage, StdStorage } from "forge-std/Test.sol";
+
+import { Consideration, ItemType, ConsiderationItem } from "../../../contracts/Consideration.sol";
 
 /// @dev Base test case that deploys Consideration and its dependencies
 contract BaseConsiderationTest is DSTestPlusPlus {
     using stdStorage for StdStorage;
 
     Consideration consideration;
-
-    ConduitController conduitController;
-    address conduitContorllerAddress;
-
-    address internal _wyvernProxyRegistry;
-    address internal _wyvernTokenTransferProxy;
-    address internal _wyvernDelegateProxyImplementation;
+    address registry;
+    address proxy;
+    address delegateProxyImplementation;
 
     function setUp() public virtual {
         _deployLegacyContracts();
-        conduitContorllerAddress = address(new ConduitController());
-        conduitController = ConduitController(conduitContorllerAddress);
 
         consideration = new Consideration(
-            conduitContorllerAddress,
-            _wyvernProxyRegistry,
-            _wyvernTokenTransferProxy,
-            _wyvernDelegateProxyImplementation
-        );
-        emit log_named_address(
-            "Deployed Consideration at",
-            address(consideration)
-        );
-    }
-
-    function singleOfferItem(
-        ItemType _itemType,
-        address _tokenAddress,
-        uint256 _identifierOrCriteria,
-        uint256 _startAmount,
-        uint256 _endAmount
-    ) internal pure returns (OfferItem[] memory offerItem) {
-        offerItem = new OfferItem[](1);
-        offerItem[0] = OfferItem(
-            _itemType,
-            _tokenAddress,
-            _identifierOrCriteria,
-            _startAmount,
-            _endAmount
-        );
-    }
-
-    function singleConsiderationItem(
-        ItemType _itemType,
-        address _tokenAddress,
-        uint256 _identifierOrCriteria,
-        uint256 _startAmount,
-        uint256 _endAmount,
-        address _recipient
-    ) internal pure returns (ConsiderationItem[] memory considerationItem) {
-        considerationItem = new ConsiderationItem[](1);
-        considerationItem[0] = ConsiderationItem(
-            _itemType,
-            _tokenAddress,
-            _identifierOrCriteria,
-            _startAmount,
-            _endAmount,
-            payable(_recipient)
+            proxy,
+            delegateProxyImplementation,
+            registry
         );
     }
 
     function signOrder(uint256 _pkOfSigner, bytes32 _orderHash)
         internal
-        returns (bytes memory)
+        returns (
+            uint8,
+            bytes32,
+            bytes32
+        )
     {
-        (bytes32 domainSeparator, ) = consideration.information();
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(
-            _pkOfSigner,
-            keccak256(
-                abi.encodePacked(bytes2(0x1901), domainSeparator, _orderHash)
-            )
-        );
-        return abi.encodePacked(r, s, v);
+        return
+            vm.sign(
+                _pkOfSigner,
+                keccak256(
+                    abi.encodePacked(
+                        bytes2(0x1901),
+                        consideration.DOMAIN_SEPARATOR(),
+                        _orderHash
+                    )
+                )
+            );
     }
 
     /**
@@ -97,40 +54,35 @@ contract BaseConsiderationTest is DSTestPlusPlus {
     function _deployLegacyContracts() private {
         /// @dev deploy WyvernProxyRegistry from precompiled source
         bytes memory bytecode = vm.getCode(
-            "out/WyvernProxyRegistry.sol/WyvernProxyRegistry.json"
+            "wyvern-0.4.13/WyvernProxyRegistry.sol/WyvernProxyRegistry.json"
         );
         // TODO: temporary, get this working before dealing with storage .slots
         address registryCopy;
         assembly {
             registryCopy := create(0, add(bytecode, 0x20), mload(bytecode))
         }
-        _wyvernProxyRegistry = registryCopy;
-
+        registry = registryCopy;
         /// @dev deploy WyvernTokenTransferProxy from precompiled source
         bytes memory constructorArgs = abi.encode(registryCopy);
         bytecode = abi.encodePacked(
             vm.getCode(
-                "out/WyvernTokenTransferProxy.sol/WyvernTokenTransferProxy.json"
+                "wyvern-0.4.13/WyvernTokenTransferProxy.sol/WyvernTokenTransferProxy.json"
             ),
             constructorArgs
         );
-        /// @dev deploy WyvernTokenTransferProxy from precompiled source
         address proxyCopy;
         assembly {
             proxyCopy := create(0, add(bytecode, 0x20), mload(bytecode))
         }
-        _wyvernTokenTransferProxy = proxyCopy;
-
+        proxy = proxyCopy;
         /// @dev use stdstore to read delegateProxyImplementation from deployed registry
-        _wyvernDelegateProxyImplementation = address(
+        delegateProxyImplementation = address(
             uint160(
                 stdstore
-                    .target(_wyvernProxyRegistry)
+                    .target(registry)
                     .sig("delegateProxyImplementation()")
                     .find()
             )
         );
-
-        emit log("Deployed legacy Wyvern contracts");
     }
 }
