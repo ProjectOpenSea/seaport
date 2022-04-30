5393a0fe611701a1d681b7ebeee6119cc7352845
Benjamin LeFevre <ben.lefevre@opensea.io>
2022-04-30 15:49:56 -0500

    Reapply DSTestPlusPlus

--- test/foundry/utils/DSTestPlusPlus.sol
+++ test/foundry/utils/DSTestPlusPlus.sol
@@ -2,7 +2,15 @@
 
 pragma solidity 0.8.13;
 
-//import { DSTestPlus } from "@rari-capital/solmate/src/test/utils/DSTestPlus.sol";
+import { DSTestPlus } from "@rari-capital/solmate/src/test/utils/DSTestPlus.sol";
 import { Test } from "forge-std/Test.sol";
 
-contract DSTestPlusPlus is Test {}
+contract DSTestPlusPlus is Test, DSTestPlus {
+    function assertFalse(bool condition)
+        internal
+        virtual
+        override(Test, DSTestPlus)
+    {
+        Test.assertFalse(condition);
+    }
+}
