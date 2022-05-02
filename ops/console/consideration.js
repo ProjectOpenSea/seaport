285cbb7304151befddc39da796657398b00072c1
bohendo <bohendo@pm.me>
2022-05-02 16:48:13 -0400

    implement cancel helper fn

--- ops/console/consideration.js
+++ ops/console/consideration.js
@@ -69,6 +69,15 @@ const createConsiderationItem = (overrides) => ({
   ...(overrides || {}),
 });
 
+const createCriteriaResolver = (overrides) => ({
+  orderIndex: 0,
+  side: 0, // Offer
+  index: 0,
+  identifier: 0,
+  criteriaProof: [],
+  ...(overrides || {}),
+});
+
 // By default, consider a payment of one ERC20 token
 const createOrderParameters = (overrides) => ({
   offerer: wallets[0].address,
@@ -85,13 +94,57 @@ const createOrderParameters = (overrides) => ({
   consideration: [createConsiderationItem(overrides?.consideration?.[0])],
 });
 
-const createCriteriaResolver = (overrides) => ({
-  orderIndex: 0,
-  side: 0, // Offer
-  index: 0,
-  identifier: 0,
-  criteriaProof: [],
+// By default, consider a payment of one ERC20 token
+const createOrderComponents = (overrides) => ({
+  offerer: wallets[0].address,
+  zone: AddressZero,
+  orderType: 0, // FULL_OPEN
+  startTime: Math.round(Date.now() / 1000) - day, // 1 day ago
+  endTime: Math.round(Date.now() / 1000) + day, // 1 day from now
+  zoneHash: HashZero,
+  salt: HashZero,
+  conduit: AddressZero,
+  nonce: 0,
   ...(overrides || {}),
+  offer: [createOfferItem(overrides?.offer?.[0])],
+  consideration: [createConsiderationItem(overrides?.consideration?.[0])],
+});
+
+// By default, consider a payment of one ERC20 token
+const createOrder = (overrides) => ({
+  parameters: createOrderParameters(overrides?.parameters),
+  signature: overrides?.signature || HashZero,
+});
+
+const createBasicOrder = (overrides) => ({
+  considerationToken: TestERC20.address,
+  considerationIdentifier: 0,
+  considerationAmount: 1,
+  offerer: wallets[0].address,
+  zone: AddressZero,
+  offerToken: TestERC721.address,
+  offerIdentifier: 0,
+  offerAmount: 1,
+  basicOrderType: 16, // ERC721_TO_ERC20_FULL_OPEN
+  startTime: Math.round(Date.now() / 1000) - day, // 1 day ago
+  endTime: Math.round(Date.now() / 1000) + day, // 1 day from now
+  zoneHash: HashZero,
+  salt: HashZero,
+  offererConduit: AddressZero,
+  fulfillerConduit: AddressZero,
+  totalOriginalAdditionalRecipients: 1,
+  additionalRecipients: [],
+  signature: HashZero,
+  ...(overrides || {}),
+});
+
+const createAdvancedOrder = (overrides) => ({
+  numerator: 1,
+  denominator: 1,
+  signature: HashZero,
+  extraData: HashZero,
+  ...(overrides?.advancedOrder || {}),
+  parameters: createOrderParameters(overrides?.params),
 });
 
 const helpers = {
@@ -100,7 +153,11 @@ const helpers = {
   createOfferItem,
   createConsiderationItem,
   createOrderParameters,
+  createOrderComponents,
   createCriteriaResolver,
+  createOrder,
+  createBasicOrder,
+  createAdvancedOrder,
 };
 
 /// /////////////////////////////////////
@@ -116,33 +173,8 @@ const helpers = {
 /// - incrementNonce
 
 const fulfillBasicOrder = async (overrides, signer = wallets[0]) => {
-  // If no orders are available, create one before fulfilling anything?
-  await new Promise((resolve) => setTimeout(resolve, 10));
-  log(``);
-  const tokenAmount = 1;
-  await mintERC20(tokenAmount, signer);
-  const nftId = overrides?.considerationIdentifier || 1337;
-  const basicOrder = {
-    ...overrides,
-    considerationToken: TestERC20.address,
-    considerationIdentifier: 0,
-    considerationAmount: 1,
-    offerer: signer.address,
-    zone: AddressZero,
-    offerToken: TestERC721.address,
-    offerIdentifier: nftId,
-    offerAmount: tokenAmount,
-    basicOrderType: 16, // ERC721_TO_ERC20_FULL_OPEN
-    startTime: Math.round(Date.now() / 1000), // seconds from epoch until now
-    endTime: Math.round(Date.now() / 1000) + 60 * 60, // 1 hour from now
-    zoneHash: HashZero,
-    salt: HashZero,
-    offererConduit: AddressZero,
-    fulfillerConduit: AddressZero,
-    totalOriginalAdditionalRecipients: 1,
-    additionalRecipients: [],
-    signature: HashZero,
-  };
+  const basicOrder = createBasicOrder(overrides);
+  await mintERC20(basicOrder.considerationAmount, signer);
   log(`Fulfilling basic order: ${JSON.stringify(basicOrder, null, 2)}`);
   Consideration.connect(signer)
     .fulfillBasicOrder(basicOrder)
@@ -153,19 +185,12 @@ const fulfillBasicOrder = async (overrides, signer = wallets[0]) => {
   log(`Success`);
 };
 
-const fulfillOrder = async (order, signer = wallets[0]) => {
-  const params = order?.parameters || {};
-  await new Promise((resolve) => setTimeout(resolve, 10));
-  log(``);
-  const tokenAmount = 1;
-  await mintERC20(tokenAmount, signer);
-  const fullOrder = {
-    parameters: createOrderParameters(params),
-    signature: order?.signature || HashZero,
-  };
-  log(`Fulfilling order: ${JSON.stringify(fullOrder, null, 2)}`);
+const fulfillOrder = async (overrides, signer = wallets[0]) => {
+  const order = createOrder(overrides);
+  await mintERC20(order.parameters.consideration[0].startAmount, signer);
+  log(`Fulfilling order: ${JSON.stringify(order, null, 2)}`);
   Consideration.connect(signer)
-    .fulfillOrder(fullOrder, AddressZero)
+    .fulfillOrder(order, AddressZero)
     .then((tx) => {
       global.hash = tx.hash;
       logEvents(tx.hash, deployments.Consideration.abi);
@@ -173,34 +198,28 @@ const fulfillOrder = async (order, signer = wallets[0]) => {
   log(`Success`);
 };
 
-// nftId = 26;
+// eg for: let nftId = 26; run the following:
 // validate({ parameters: { offer: [{ itemType: 4, identifierOrCriteria: nftId }] } })
-// fulfillAdvancedOrder({ advOrder: { parameters: { offer: [{ itemType: 4, identifierOrCriteria: nftId }] } }, criteriaResolvers: [{ identifier: nftId }] })
+// fulfillAdvancedOrder({ advancedOrder: { parameters: { offer: [{ itemType: 4, identifierOrCriteria: nftId }] } }, criteriaResolvers: [{ identifier: nftId }] })
 const fulfillAdvancedOrder = async (overrides, signer = wallets[0]) => {
-  const params = overrides?.advOrder?.parameters || {};
-  const tokenAmount = 1;
-  await mintERC20(tokenAmount, signer);
-  const advOrder = {
-    numerator: 1,
-    denominator: 1,
-    signature: HashZero,
-    extraData: HashZero,
-    ...(overrides?.advOrder || {}),
-    parameters: createOrderParameters(params),
-  };
+  const advancedOrder = createAdvancedOrder(overrides.advancedOrder);
   const criteriaResolvers = [
     createCriteriaResolver(overrides?.criteriaResolvers?.[0]),
   ];
   const fulfillerConduit = overrides?.fulfillerConduit || AddressZero;
+  await mintERC20(
+    advancedOrder.parameters.consideration[0].startAmount,
+    signer
+  );
   log(
     `Fulfilling advanced order: ${JSON.stringify(
-      { advOrder, criteriaResolvers, fulfillerConduit },
+      { advancedOrder, criteriaResolvers, fulfillerConduit },
       null,
       2
     )}`
   );
   await Consideration.connect(signer)
-    .fulfillAdvancedOrder(advOrder, criteriaResolvers, fulfillerConduit)
+    .fulfillAdvancedOrder(advancedOrder, criteriaResolvers, fulfillerConduit)
     .then((tx) => {
       global.hash = tx.hash;
       return logEvents(tx.hash, deployments.Consideration.abi);
@@ -208,40 +227,31 @@ const fulfillAdvancedOrder = async (overrides, signer = wallets[0]) => {
   log(`Success`);
 };
 
-const fulfillAvailableAdvancedOrders = () => {};
-const matchOrders = () => {};
-const matchAdvancedOrders = () => {};
-const cancel = () => {};
+const fulfillAvailableAdvancedOrders = async () => {};
+const matchOrders = async () => {};
+const matchAdvancedOrders = async () => {};
+
+const cancel = async (overrides, signer = wallets[0]) => {
+  const order = createOrderComponents(overrides);
+  log(`Cancelling order: ${JSON.stringify(order, null, 2)}`);
+  await Consideration.connect(signer)
+    .cancel([order])
+    .then((tx) => {
+      global.hash = tx.hash;
+      return logEvents(tx.hash, deployments.Consideration.abi);
+    });
+  log(`Success`);
+};
 
 // Only one offer & consideration supported (for now)
 // Validates the sale of an NFT in exchange for some tokens by default
 // to validate an advanced order: use keccak256(nftId) for the identifierOrCriteria
-const validate = async (order, signer = wallets[0]) => {
-  const parameters = order?.parameters || {};
-  const nftId = parameters.offer?.[0]?.identifierOrCriteria || 1337;
-  await new Promise((resolve) => setTimeout(resolve, 10));
-  log(``);
-  await mintERC721(nftId, signer);
-  const fullOrder = {
-    parameters: {
-      offerer: signer.address,
-      zone: AddressZero,
-      orderType: 1,
-      startTime: Math.round(Date.now() / 1000), // seconds from epoch until now
-      endTime: Math.round(Date.now() / 1000) + 60 * 60, // 1 hour from now
-      zoneHash: HashZero,
-      salt: HashZero,
-      conduit: AddressZero,
-      totalOriginalConsiderationItems: 1,
-      ...parameters,
-      offer: [createOfferItem(parameters?.offer?.[0])],
-      consideration: [createConsiderationItem(parameters?.consideration?.[0])],
-    },
-    signature: order?.signature || HashZero,
-  };
-  log(`Validating order: ${JSON.stringify(fullOrder, null, 2)}`);
+const validate = async (overrides, signer = wallets[0]) => {
+  const order = createOrder(overrides);
+  await mintERC721(order.parameters.offer[0].identifierOrCriteria, signer);
+  log(`Validating order: ${JSON.stringify(order, null, 2)}`);
   await Consideration.connect(signer)
-    .validate([fullOrder])
+    .validate([order])
     .then((tx) => {
       global.hash = tx.hash;
       return logEvents(tx.hash, deployments.Consideration.abi);
@@ -250,7 +260,7 @@ const validate = async (order, signer = wallets[0]) => {
 };
 
 const incrementNonce = (signer = wallets[0]) => {
-  deployments.Consideration.connect(signer)
+  Consideration.connect(signer)
     .incrementNonce()
     .then((tx) => {
       global.hash = tx.hash;
