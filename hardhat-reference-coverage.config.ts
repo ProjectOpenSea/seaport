0fcb624631596a99d4ed52d259bb99f9d2947dd8
0age <0age@protonmail.com>
2022-05-03 16:44:03 -0700

    set up coverage tests for reference contracts

---
+++ hardhat-reference-coverage.config.ts
@@ -0,0 +1,39 @@
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+
+dotenv.config();
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.13",
+        settings: {
+          viaIR: false,
+          optimizer: {
+            enabled: false,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    hardhat: {
+      blockGasLimit: 30_000_000,
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+};
+
+export default config;
