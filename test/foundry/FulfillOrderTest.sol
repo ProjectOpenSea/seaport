20f5400bfa34027be3c92a6d63a2a018c0c779e6
Stephan Min <stephankmin@gmail.com>
2022-05-04 16:46:33 -0700

    modified test functions to take in structs

--- test/foundry/FulfillOrderTest.sol
+++ test/foundry/FulfillOrderTest.sol
@@ -14,28 +14,49 @@ import { ProxyRegistry } from "./interfaces/ProxyRegistry.sol";
 import { OwnableDelegateProxy } from "./interfaces/OwnableDelegateProxy.sol";
 
 contract FulfillOrderTest is BaseOrderTest {
-    function testSingleERC721(
-        address _zone,
-        uint256 _id,
-        bytes32 _zoneHash,
-        uint256 _salt,
-        uint128[3] memory _ethAmts,
-        bool _useConduit
-    ) public onlyPayable(_zone) topUp {
-        vm.assume(_ethAmts[0] > 0 && _ethAmts[1] > 0 && _ethAmts[2] > 0);
+    struct EthToERC721Struct {
+        address zone;
+        uint256 id;
+        bytes32 zoneHash;
+        uint256 salt;
+        uint128[3] ethAmts;
+        bool useConduit;
+    }
+
+    function testFulfillOrderEthToERC721(
+        EthToERC721Struct memory _ethToERC721Struct
+    ) public {
+        _testFulfillOrderEthToERC721(consideration, _ethToERC721Struct);
+        _testFulfillOrderEthToERC721(
+            Consideration(address(referenceConsideration)),
+            _ethToERC721Struct
+        );
+    }
+
+    function _testFulfillOrderEthToERC721(
+        Consideration _consideration,
+        EthToERC721Struct memory _ethToERC721
+    ) public onlyPayable(_ethToERC721.zone) topUp {
         vm.assume(
-            uint256(_ethAmts[0]) +
-                uint256(_ethAmts[1]) +
-                uint256(_ethAmts[2]) <=
+            _ethToERC721.ethAmts[0] > 0 &&
+                _ethToERC721.ethAmts[1] > 0 &&
+                _ethToERC721.ethAmts[2] > 0
+        );
+        vm.assume(
+            uint256(_ethToERC721.ethAmts[0]) +
+                uint256(_ethToERC721.ethAmts[1]) +
+                uint256(_ethToERC721.ethAmts[2]) <=
                 2**128 - 1
         );
-        bytes32 conduitKey = _useConduit ? conduitKeyOne : bytes32(0);
+        bytes32 conduitKey = _ethToERC721.useConduit
+            ? conduitKeyOne
+            : bytes32(0);
 
-        test721_1.mint(alice, _id);
+        test721_1.mint(alice, _ethToERC721.id);
         OfferItem[] memory offerItem = singleOfferItem(
             ItemType.ERC721,
             address(test721_1),
-            _id,
+            _ethToERC721.id,
             1,
             1
         );
@@ -46,121 +67,112 @@ contract FulfillOrderTest is BaseOrderTest {
             ItemType.NATIVE,
             address(0),
             0,
-            uint256(_ethAmts[0]),
-            uint256(_ethAmts[0]),
+            uint256(_ethToERC721.ethAmts[0]),
+            uint256(_ethToERC721.ethAmts[0]),
             payable(alice)
         );
         considerationItems[1] = ConsiderationItem(
             ItemType.NATIVE,
             address(0),
             0,
-            uint256(_ethAmts[1]),
-            uint256(_ethAmts[1]),
-            payable(_zone) // TODO: should we fuzz on zone? do royalties get paid to zone??
+            uint256(_ethToERC721.ethAmts[1]),
+            uint256(_ethToERC721.ethAmts[1]),
+            payable(_ethToERC721.zone) // TODO: should we fuzz on zone? do royalties get paid to zone??
         );
         considerationItems[2] = ConsiderationItem(
             ItemType.NATIVE,
             address(0),
             0,
-            uint256(_ethAmts[2]),
-            uint256(_ethAmts[2]),
+            uint256(_ethToERC721.ethAmts[2]),
+            uint256(_ethToERC721.ethAmts[2]),
             payable(cal)
         );
 
         OrderComponents memory orderComponents = OrderComponents(
             alice,
-            _zone,
+            _ethToERC721.zone,
             offerItem,
             considerationItems,
             OrderType.FULL_OPEN,
             block.timestamp,
             block.timestamp + 1,
-            _zoneHash,
-            _salt,
+            _ethToERC721.zoneHash,
+            _ethToERC721.salt,
             conduitKey,
-            consideration.getNonce(alice)
+            _consideration.getNonce(alice)
         );
         bytes memory signature = signOrder(
-            consideration,
+            _consideration,
             alicePk,
-            consideration.getOrderHash(orderComponents)
+            _consideration.getOrderHash(orderComponents)
         );
         OrderParameters memory orderParameters = OrderParameters(
             address(alice),
-            _zone,
+            _ethToERC721.zone,
             offerItem,
             considerationItems,
             OrderType.FULL_OPEN,
             block.timestamp,
             block.timestamp + 1,
-            _zoneHash,
-            _salt,
+            _ethToERC721.zoneHash,
+            _ethToERC721.salt,
             conduitKey,
             considerationItems.length
         );
-        consideration.fulfillOrder{
-            value: _ethAmts[0] + _ethAmts[1] + _ethAmts[2]
+        _consideration.fulfillOrder{
+            value: _ethToERC721.ethAmts[0] +
+                _ethToERC721.ethAmts[1] +
+                _ethToERC721.ethAmts[2]
         }(Order(orderParameters, signature), conduitKey);
     }
 
+    struct EthToERC1155Struct {
+        address zone;
+        uint256 id;
+        uint256 amount;
+        bytes32 zoneHash;
+        uint256 salt;
+        uint128[3] ethAmts;
+        bool useConduit;
+    }
+
     function testFulfillOrderEthToERC1155(
-        address _zone,
-        uint256 _id,
-        uint256 _amount,
-        bytes32 _zoneHash,
-        uint256 _salt,
-        uint128[3] memory _ethAmts,
-        bool _useConduit
+        EthToERC1155Struct memory _ethToERC1155Struct
     ) public {
-        _testFulfillOrderEthToERC1155(
-            consideration,
-            _zone,
-            _id,
-            _amount,
-            _zoneHash,
-            _salt,
-            _ethAmts,
-            _useConduit
-        );
+        _testFulfillOrderEthToERC1155(consideration, _ethToERC1155Struct);
         _testFulfillOrderEthToERC1155(
             Consideration(address(referenceConsideration)),
-            _zone,
-            _id,
-            _amount,
-            _zoneHash,
-            _salt,
-            _ethAmts,
-            _useConduit
+            _ethToERC1155Struct
         );
     }
 
     function _testFulfillOrderEthToERC1155(
         Consideration _consideration,
-        address _zone,
-        uint256 _id,
-        uint256 _amount,
-        bytes32 _zoneHash,
-        uint256 _salt,
-        uint128[3] memory _ethAmts,
-        bool _useConduit
-    ) public onlyPayable(_zone) topUp {
-        vm.assume(_amount > 0);
-        vm.assume(_ethAmts[0] > 0 && _ethAmts[1] > 0 && _ethAmts[2] > 0);
+        EthToERC1155Struct memory _ethToERC1155
+    ) public onlyPayable(_ethToERC1155.zone) topUp {
+        vm.assume(_ethToERC1155.amount > 0);
         vm.assume(
-            uint256(_ethAmts[0]) +
-                uint256(_ethAmts[1]) +
-                uint256(_ethAmts[2]) <=
+            _ethToERC1155.ethAmts[0] > 0 &&
+                _ethToERC1155.ethAmts[1] > 0 &&
+                _ethToERC1155.ethAmts[2] > 0
+        );
+        vm.assume(
+            uint256(_ethToERC1155.ethAmts[0]) +
+                uint256(_ethToERC1155.ethAmts[1]) +
+                uint256(_ethToERC1155.ethAmts[2]) <=
                 2**128 - 1
         );
-        bytes32 conduitKey = _useConduit ? conduitKeyOne : bytes32(0);
+        bytes32 conduitKey = _ethToERC1155.useConduit
+            ? conduitKeyOne
+            : bytes32(0);
 
-        test1155_1.mint(alice, _id, _amount);
+        test1155_1.mint(alice, _ethToERC1155.id, _ethToERC1155.amount);
         OfferItem[] memory offerItem = singleOfferItem(
             ItemType.ERC1155,
             address(test1155_1),
-            _id,
-            _amount,
-            _amount
+            _ethToERC1155.id,
+            _ethToERC1155.amount,
+            _ethToERC1155.amount
         );
 
         ConsiderationItem[] memory considerationItems = new ConsiderationItem[](
@@ -170,60 +182,66 @@ contract FulfillOrderTest is BaseOrderTest {
             ItemType.NATIVE,
             address(0),
             0,
-            uint256(_ethAmts[0]),
-            uint256(_ethAmts[0]),
+            uint256(_ethToERC1155.ethAmts[0]),
+            uint256(_ethToERC1155.ethAmts[0]),
             payable(alice)
         );
         considerationItems[1] = ConsiderationItem(
             ItemType.NATIVE,
             address(0),
             0,
-            uint256(_ethAmts[1]),
-            uint256(_ethAmts[1]),
-            payable(_zone) // TODO: should we fuzz on zone? do royalties get paid to zone??
+            uint256(_ethToERC1155.ethAmts[1]),
+            uint256(_ethToERC1155.ethAmts[1]),
+            payable(_ethToERC1155.zone) // TODO: should we fuzz on zone? do royalties get paid to zone??
         );
         considerationItems[2] = ConsiderationItem(
             ItemType.NATIVE,
             address(0),
             0,
-            uint256(_ethAmts[2]),
-            uint256(_ethAmts[2]),
+            uint256(_ethToERC1155.ethAmts[2]),
+            uint256(_ethToERC1155.ethAmts[2]),
             payable(cal)
         );
 
         OrderComponents memory orderComponents = OrderComponents(
             alice,
-            _zone,
+            _ethToERC1155.zone,
             offerItem,
             considerationItems,
             OrderType.FULL_OPEN,
             block.timestamp,
             block.timestamp + 1,
-            _zoneHash,
-            _salt,
+            _ethToERC1155.zoneHash,
+            _ethToERC1155.salt,
             conduitKey,
-            consideration.getNonce(alice)
+            _consideration.getNonce(alice)
         );
         bytes memory signature = signOrder(
+<<<<<<< HEAD
             consideration,
+=======
+            _consideration,
+>>>>>>> f71f62e (modified test functions to take in structs)
             alicePk,
-            consideration.getOrderHash(orderComponents)
+            _consideration.getOrderHash(orderComponents)
         );
         OrderParameters memory orderParameters = OrderParameters(
             address(alice),
-            _zone,
+            _ethToERC1155.zone,
             offerItem,
             considerationItems,
             OrderType.FULL_OPEN,
             block.timestamp,
             block.timestamp + 1,
-            _zoneHash,
-            _salt,
+            _ethToERC1155.zoneHash,
+            _ethToERC1155.salt,
             conduitKey,
             considerationItems.length
         );
         _consideration.fulfillOrder{
-            value: _ethAmts[0] + _ethAmts[1] + _ethAmts[2]
+            value: _ethToERC1155.ethAmts[0] +
+                _ethToERC1155.ethAmts[1] +
+                _ethToERC1155.ethAmts[2]
         }(Order(orderParameters, signature), conduitKey);
     }
 
@@ -238,22 +256,23 @@ contract FulfillOrderTest is BaseOrderTest {
     //     bool _useConduit
     // ) public onlyPayble(_zone) topUp {}
 
-    function testFailSingleERC721NonPayableZone() public {
-        // fuzzer completely ignores params that fail vm.assume,
-        // so confirm that this invalid param does fail
-        testSingleERC721(
-            address(test721_1),
-            11091106379292436006407652670679179902088932118277811194146176427645468672,
-            0x00000000000000000000000000000000000657137d140ac73d37f3c9267132fd,
-            11737039766497076565,
-            [
-                1329227995784915872903807060280344576,
-                2941925602480186165443492,
-                276220320249354716809723209050602608289
-            ],
-            false
-        );
-    }
+    // function testFailSingleERC721NonPayableZone() public {
+    //     // fuzzer completely ignores params that fail vm.assume,
+    //     // so confirm that this invalid param does fail
+    //     testSingleERC721(
+    //         consideration,
+    //         address(test721_1),
+    //         11091106379292436006407652670679179902088932118277811194146176427645468672,
+    //         0x00000000000000000000000000000000000657137d140ac73d37f3c9267132fd,
+    //         11737039766497076565,
+    //         [
+    //             1329227995784915872903807060280344576,
+    //             2941925602480186165443492,
+    //             276220320249354716809723209050602608289
+    //         ],
+    //         false
+    //     );
+    // }
 
     function getMaxConsiderationValue(
         ConsiderationItem[] memory considerationItems
