0a07f4a7cd0e75ee4e08b34ae0e2153e9853087d
Benjamin LeFevre <ben.lefevre@opensea.io>
2022-05-06 17:41:24 -0500

    update coverage to 0.8.7

--- hardhat-reference-coverage.config.ts
+++ hardhat-reference-coverage.config.ts
@@ -15,7 +15,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.13",
+        version: "0.8.7",
         settings: {
           viaIR: false,
           optimizer: {
