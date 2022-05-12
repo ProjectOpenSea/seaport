f5546942f347fe4f5c2f5e7b43d6b69c912771fb
0age <0age@protonmail.com>
2022-05-11 21:01:30 -0700

    add basic conduit 1155 batch transfer test

--- test/index.js
+++ test/index.js
@@ -11781,6 +11781,68 @@ describe(`Consideration (version: ${VERSION}) — initial test suite`, function 
       );
     });
 
+    it("Deploys a conduit, adds a channel, and executes transfers", async () => {
+      const tempConduitKey =
+        "0xff00000000000000000000dd" + owner.address.slice(2);
+
+      const { conduit: tempConduitAddress } =
+        await conduitController.getConduit(tempConduitKey);
+
+      await whileImpersonating(owner.address, provider, async () => {
+        await conduitController
+          .connect(owner)
+          .createConduit(tempConduitKey, owner.address);
+      });
+
+      const tempConduit = conduitImplementation.attach(tempConduitAddress);
+
+      await whileImpersonating(owner.address, provider, async () => {
+        await conduitController
+          .connect(owner)
+          .updateChannel(tempConduit.address, owner.address, true);
+      });
+
+      const nftId = ethers.BigNumber.from(randomHex().slice(0, 10));
+      const amount = ethers.BigNumber.from(randomHex().slice(0, 10)).add(1);
+      await testERC1155.mint(owner.address, nftId, amount.mul(2));
+
+      const secondNftId = ethers.BigNumber.from(randomHex().slice(0, 10));
+      const secondAmount = ethers.BigNumber.from(randomHex().slice(0, 10)).add(
+        1
+      );
+      await testERC1155.mint(owner.address, secondNftId, secondAmount.mul(2));
+
+      await whileImpersonating(owner.address, provider, async () => {
+        await expect(
+          testERC1155
+            .connect(owner)
+            .setApprovalForAll(tempConduit.address, true)
+        )
+          .to.emit(testERC1155, "ApprovalForAll")
+          .withArgs(owner.address, tempConduit.address, true);
+      });
+
+      await tempConduit.connect(owner).executeWithBatch1155(
+        [],
+        [
+          {
+            token: testERC1155.address,
+            from: owner.address,
+            to: buyer.address,
+            ids: [nftId, secondNftId],
+            amounts: [amount, secondAmount],
+          },
+          {
+            token: testERC1155.address,
+            from: owner.address,
+            to: buyer.address,
+            ids: [secondNftId, nftId],
+            amounts: [secondAmount, amount],
+          },
+        ]
+      );
+    });
+
     it("Reverts when attempting to update a conduit channel when call is not from controller", async () => {
       await whileImpersonating(owner.address, provider, async () => {
         await expect(
