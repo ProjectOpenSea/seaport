4c1b395c4154dd1c60bf35784690a9b4ac1f06e9
James Wenzel <james.wenzel@opensea.io>
2022-05-13 16:09:52 -0700

    test execute passes for reference

--- test/foundry/conduit/ConduitExecute.t.sol
+++ test/foundry/conduit/ConduitExecute.t.sol
@@ -21,21 +21,75 @@ contract ConduitExecuteTest is BaseConduitTest {
         ConduitTransfer[] transfers;
     }
 
-    ConduitTransfer[] transfers;
+    mapping(address => mapping(address => mapping(uint256 => uint256))) userToExpectedTokenIdentifierBalance;
+
+    function updateExpectedBalance(ConduitTransfer memory transfer) internal {
+        userToExpectedTokenIdentifierBalance[transfer.to][transfer.token][
+            transfer.identifier
+        ] += transfer.amount;
+    }
+
+    function preprocessTransfers(ConduitTransfer[] memory transfers) internal {
+        for (uint256 i = 0; i < transfers.length; i++) {
+            ConduitTransfer memory transfer = transfers[i];
+            ConduitItemType itemType = transfer.itemType;
+            if (itemType != ConduitItemType.ERC721) {
+                updateExpectedBalance(transfers[i]);
+            }
+        }
+    }
 
     function testExecute(FuzzInputs memory inputs) public {
+        ConduitTransfer[] memory transfers = new ConduitTransfer[](
+            inputs.intermediates.length
+        );
         for (uint8 i; i < inputs.intermediates.length; i++) {
-            transfers.push(
-                createTokenAndConduitTransfer(
-                    inputs.intermediates[i],
-                    address(referenceConduit)
-                )
+            transfers[i] = createTokenAndConduitTransfer(
+                inputs.intermediates[i],
+                address(referenceConduit)
             );
         }
+        preprocessTransfers(transfers);
         _testExecute(Context(referenceConduit, transfers));
     }
 
+    function _expectedBalance(ConduitTransfer memory transfer)
+        internal
+        view
+        returns (uint256)
+    {
+        return
+            userToExpectedTokenIdentifierBalance[transfer.to][transfer.token][
+                transfer.identifier
+            ];
+    }
+
     function _testExecute(Context memory context) internal {
-        context.conduit.execute(context.transfers);
+        bytes4 magicValue = context.conduit.execute(context.transfers);
+        assertEq(magicValue, Conduit.execute.selector);
+
+        for (uint256 i; i < context.transfers.length; i++) {
+            ConduitTransfer memory transfer = context.transfers[i];
+            ConduitItemType itemType = transfer.itemType;
+            if (itemType == ConduitItemType.ERC20) {
+                assertEq(
+                    TestERC20(transfer.token).balanceOf(transfer.to),
+                    _expectedBalance(transfer)
+                );
+            } else if (itemType == ConduitItemType.ERC1155) {
+                assertEq(
+                    TestERC1155(transfer.token).balanceOf(
+                        transfer.to,
+                        transfer.identifier
+                    ),
+                    _expectedBalance(transfer)
+                );
+            } else if (itemType == ConduitItemType.ERC721) {
+                assertEq(
+                    TestERC721(transfer.token).ownerOf(transfer.identifier),
+                    transfer.to
+                );
+            }
+        }
     }
 }
