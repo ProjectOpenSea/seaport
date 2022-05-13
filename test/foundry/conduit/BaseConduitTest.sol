6b51fd6f59047a157324f9693307b6bed3aa2521
James Wenzel <james.wenzel@opensea.io>
2022-05-13 15:01:42 -0700

    add createTokenAndConduitTransfer

--- test/foundry/conduit/BaseConduitTest.sol
+++ test/foundry/conduit/BaseConduitTest.sol
@@ -15,9 +15,28 @@ contract BaseConduitTest is
     ERC1155Recipient,
     ERC721Recipient
 {
-    address[] erc20s;
-    address[] erc721s;
-    address[] erc1155s;
+    TestERC20[] erc20s;
+    TestERC721[] erc721s;
+    TestERC1155[] erc1155s;
+
+    struct ConduitTransferIntermediate {
+        uint8 itemType;
+        address from;
+        address to;
+        uint256 identifier;
+        // uint128 so minting won't overflow if same erc1155 ids are minted
+        uint128 amount;
+    }
+
+    function setUp() public override {
+        super.setUp();
+        conduitController.updateChannel(conduit, address(this), true);
+        referenceConduitController.updateChannel(
+            referenceConduit,
+            address(this),
+            true
+        );
+    }
 
     function isErc1155Receiver(address to) internal returns (bool success) {
         success = true;
@@ -35,7 +54,7 @@ contract BaseConduitTest is
         }
     }
 
-    ///@dev helper to turn a fuzzed address
+    ///@dev helper to make sure fuzzed addresses can receive erc1155s by changing it if it can't
     function receiver(address to) internal returns (address) {
         if (!isErc1155Receiver(to)) {
             if (uint160(to) == 2**160 - 1) {
@@ -46,13 +65,61 @@ contract BaseConduitTest is
         return to;
     }
 
-    function setUp() public override {
-        super.setUp();
-        conduitController.updateChannel(conduit, address(this), true);
-        referenceConduitController.updateChannel(
-            referenceConduit,
-            address(this),
-            true
+    function createTokenAndConduitTransfer(
+        ConduitTransferIntermediate memory intermediate,
+        address currentConduit
+    ) internal returns (ConduitTransfer memory) {
+        ConduitTransfer memory conduitTransfer;
+        ConduitItemType itemType = ConduitItemType(
+            (intermediate.itemType % 3) + 1
         );
+        address from = receiver(intermediate.from);
+        address to = receiver(intermediate.to);
+        if (itemType == ConduitItemType.ERC20) {
+            TestERC20 erc20 = new TestERC20();
+            erc20.mint(from, intermediate.amount);
+            vm.prank(from);
+            erc20.approve(currentConduit, 2**256 - 1);
+            erc20s.push(erc20);
+            return
+                ConduitTransfer(
+                    itemType,
+                    address(erc20),
+                    from,
+                    to,
+                    0,
+                    intermediate.amount
+                );
+        } else if (itemType == ConduitItemType.ERC1155) {
+            TestERC1155 erc1155 = new TestERC1155();
+            erc1155.mint(from, intermediate.identifier, intermediate.amount);
+            vm.prank(from);
+            erc1155.setApprovalForAll(currentConduit, true);
+            erc1155s.push(erc1155);
+            return
+                ConduitTransfer(
+                    itemType,
+                    address(erc1155),
+                    from,
+                    to,
+                    intermediate.identifier,
+                    intermediate.amount
+                );
+        } else if (itemType == ConduitItemType.ERC721) {
+            TestERC721 erc721 = new TestERC721();
+            erc721.mint(from, intermediate.identifier);
+            vm.prank(from);
+            erc721.setApprovalForAll(currentConduit, true);
+            erc721s.push(erc721);
+            return
+                ConduitTransfer(
+                    itemType,
+                    address(erc721),
+                    from,
+                    to,
+                    intermediate.identifier,
+                    1
+                );
+        }
     }
 }
