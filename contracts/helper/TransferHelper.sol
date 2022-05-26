b97bc2cbfd6cdd5a83a87ef6076fbec34d1e5f6a
stephankmin <stephankmin@gmail.com>
2022-05-25 19:15:30 -0700

    progress on TransferHelper

--- contracts/helper/TransferHelper.sol
+++ contracts/helper/TransferHelper.sol
@@ -2,19 +2,88 @@
 pragma solidity 0.8.13;
 
 import "./TransferHelperStructs.sol";
+import { TokenTransferrer } from "../lib/TokenTransferrer.sol";
+import { ConduitControllerInterface } from "../interfaces/ConduitControllerInterface.sol";
+import { ConduitTransfer } from "../conduit/lib/ConduitStructs.sol";
 
 /**
  * @title TransferHelper
  * @author stuckinaboot, stephankmin
  * @notice TransferHelper is a trivial ETH/ERC20/ERC721/ERC1155 marketplace
  */
-contract TransferHelper {
-    function bulkTransfer() public returns (bool) {
-        TransferHelperItem memory item = TransferHelperItem({
-            token: address(0),
-            tokenIdentifier: 0,
-            amount: 0
-        });
-        // TODO do something
+contract TransferHelper is TokenTransferrer {
+    // Allow for interaction with the conduit controller.
+    ConduitControllerInterface internal immutable _CONDUIT_CONTROLLER;
+
+    // Cache the conduit creation code hash used by the conduit controller.
+    bytes32 internal immutable _CONDUIT_CREATION_CODE_HASH;
+
+    constructor(address conduitController) {
+        // Set the supplied conduit controller.
+        _CONDUIT_CONTROLLER = ConduitControllerInterface(conduitController);
+
+        // Retrieve the conduit creation code hash from the supplied controller.
+        (_CONDUIT_CREATION_CODE_HASH, ) = (
+            _CONDUIT_CONTROLLER.getConduitCodeHashes()
+        );
+    }
+
+    function bulkTransfer(
+        TransferHelperItem[] calldata items,
+        address recipient,
+        bytes32 conduitKey
+    ) public returns (bool) {
+        // if no conduit, call TokenTransferrer
+        if (conduitKey == 0) {
+            for (uint256 i; i < items.length; i++) {
+                TransferHelperItem calldata item = items[i];
+                if (item.itemType == ConduitItemType.NATIVE) {
+                    continue;
+                } else if (item.itemType == ConduitItemType.ERC20) {
+                    _performERC20Transfer(
+                        item.token,
+                        msg.sender,
+                        recipient,
+                        item.amount
+                    );
+                } else if (item.itemType == ConduitItemType.ERC721) {
+                    _performERC721Transfer(
+                        item.token,
+                        msg.sender,
+                        recipient,
+                        item.tokenIdentifier
+                    );
+                } else {
+                    // do we have to check for ERC721 and ERC1155 with criteria?
+                    _performERC1155Transfer(
+                        item.token,
+                        msg.sender,
+                        recipient,
+                        item.tokenIdentifier,
+                        item.amount
+                    );
+                }
+            }
+        }
+        // if conduit, derive conduit address from key
+        else {
+            (address conduit, ) = _CONDUIT_CONTROLLER.getConduit(conduitKey);
+            ConduitTransfer[] memory conduitTransfers = new ConduitTransfer[](
+                items.length
+            );
+            // modify TransferHelperItems to ConduitTranfsers
+            for (uint256 i; i < items.length; i++) {
+                TransferHelperItem calldata item = items[i];
+                conduitTransfers[i] = ConduitTransfer(
+                    item.itemType,
+                    item.token,
+                    msg.sender,
+                    recipient,
+                    item.tokenIdentifier,
+                    item.amount
+                );
+            }
+            conduit.execute(conduitTransfers); // TODO: pass in conduitTransfers as calldata
+        }
     }
 }
--- contracts/helper/TransferHelperStructs.sol
+++ contracts/helper/TransferHelperStructs.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
+import { ConduitItemType } from "../conduit/lib/ConduitEnums.sol";
+
 struct TransferHelperItem {
+    ConduitItemType itemType;
     address token;
     uint256 tokenIdentifier;
     uint256 amount;
