a5df1e001c84316fffc0615648ca9bad8ac197b5
Aspyn Palatnick <aspyn.palatnick@joinozone.com>
2022-06-04 10:09:33 -0400

    rm unused code

--- contracts/helper/TransferHelper.sol
+++ contracts/helper/TransferHelper.sol
@@ -152,21 +152,4 @@ contract TransferHelper is TransferHelperInterface, TokenTransferrer {
         // Return a magic value indicating that the transfers were performed.
         magicValue = this.bulkTransfer.selector;
     }
-
-    function toString(address account) public pure returns (string memory) {
-        return toString(abi.encodePacked(account));
-    }
-
-    function toString(bytes memory data) public pure returns (string memory) {
-        bytes memory alphabet = "0123456789abcdef";
-
-        bytes memory str = new bytes(2 + data.length * 2);
-        str[0] = "0";
-        str[1] = "x";
-        for (uint256 i = 0; i < data.length; i++) {
-            str[2 + i * 2] = alphabet[uint256(uint8(data[i] >> 4))];
-            str[3 + i * 2] = alphabet[uint256(uint8(data[i] & 0x0f))];
-        }
-        return string(str);
-    }
 }
