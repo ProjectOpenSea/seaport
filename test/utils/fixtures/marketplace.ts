8614836194622610729c2a42349436c8838951a1
d1ll0n <dillonkellar@protonmail.com>
2022-06-04 00:49:07 -0500

    Add fixtures for all contracts

---
+++ test/utils/fixtures/marketplace.ts
@@ -0,0 +1,474 @@
+import { expect } from "chai";
+import { constants, Wallet } from "ethers";
+import { keccak256, recoverAddress } from "ethers/lib/utils";
+import hre, { ethers } from "hardhat";
+import {
+  Conduit,
+  ConduitController,
+  ImmutableCreate2FactoryInterface,
+  Seaport,
+  TestZone,
+} from "../../../typechain-types";
+import { deployContract } from "../contracts";
+import {
+  calculateOrderHash,
+  convertSignatureToEIP2098,
+  randomHex,
+  toBN,
+} from "../encoding";
+import {
+  AdvancedOrder,
+  ConsiderationItem,
+  CriteriaResolver,
+  OfferItem,
+  OrderComponents,
+} from "../types";
+
+const { orderType } = require("../../../eip-712-types/order");
+const deployConstants = require("../../../constants/constants");
+
+const VERSION = !process.env.REFERENCE ? "1" : "rc.1";
+
+export async function marketplaceFixture(
+  create2Factory: ImmutableCreate2FactoryInterface,
+  conduitController: ConduitController,
+  conduitOne: Conduit,
+  chainId: number,
+  owner: Wallet
+) {
+  // Deploy marketplace contract through efficient create2 factory
+  const marketplaceContractFactory = await ethers.getContractFactory(
+    process.env.REFERENCE ? "ReferenceConsideration" : "Seaport"
+  );
+
+  const directMarketplaceContract = await deployContract(
+    process.env.REFERENCE ? "ReferenceConsideration" : "Consideration",
+    owner as any,
+    conduitController.address
+  );
+
+  const marketplaceContractAddress = await create2Factory.findCreate2Address(
+    deployConstants.MARKETPLACE_CONTRACT_CREATION_SALT,
+    marketplaceContractFactory.bytecode +
+      conduitController.address.slice(2).padStart(64, "0")
+  );
+
+  let { gasLimit } = await ethers.provider.getBlock("latest");
+
+  if ((hre as any).__SOLIDITY_COVERAGE_RUNNING) {
+    gasLimit = ethers.BigNumber.from(300_000_000);
+  }
+
+  await create2Factory.safeCreate2(
+    deployConstants.MARKETPLACE_CONTRACT_CREATION_SALT,
+    marketplaceContractFactory.bytecode +
+      conduitController.address.slice(2).padStart(64, "0"),
+    {
+      gasLimit,
+    }
+  );
+
+  const marketplaceContract = (await ethers.getContractAt(
+    process.env.REFERENCE ? "ReferenceConsideration" : "Seaport",
+    marketplaceContractAddress,
+    owner
+  )) as Seaport;
+
+  await conduitController
+    .connect(owner)
+    .updateChannel(conduitOne.address, marketplaceContract.address, true);
+
+  const stubZone: TestZone = await deployContract("TestZone", owner as any);
+
+  // Required for EIP712 signing
+  const domainData = {
+    name: process.env.REFERENCE ? "Consideration" : "Seaport",
+    version: VERSION,
+    chainId: chainId,
+    verifyingContract: marketplaceContract.address,
+  };
+
+  const getAndVerifyOrderHash = async (orderComponents: OrderComponents) => {
+    const orderHash = await marketplaceContract.getOrderHash(
+      orderComponents as any
+    );
+    const derivedOrderHash = calculateOrderHash(orderComponents);
+    expect(orderHash).to.equal(derivedOrderHash);
+    return orderHash;
+  };
+
+  // Returns signature
+  const signOrder = async (
+    orderComponents: OrderComponents,
+    signer: Wallet
+  ) => {
+    const signature = await signer._signTypedData(
+      domainData,
+      orderType,
+      orderComponents
+    );
+
+    const orderHash = await getAndVerifyOrderHash(orderComponents);
+
+    const { domainSeparator } = await marketplaceContract.information();
+    const digest = keccak256(
+      `0x1901${domainSeparator.slice(2)}${orderHash.slice(2)}`
+    );
+    const recoveredAddress = recoverAddress(digest, signature);
+
+    expect(recoveredAddress).to.equal(signer.address);
+
+    return signature;
+  };
+
+  const createOrder = async (
+    offerer: Wallet,
+    zone: Wallet | undefined = undefined,
+    offer: OfferItem[],
+    consideration: ConsiderationItem[],
+    orderType: number,
+    criteriaResolvers?: CriteriaResolver[],
+    timeFlag?: string,
+    signer?: Wallet,
+    zoneHash = constants.HashZero,
+    conduitKey = constants.HashZero,
+    extraCheap = false
+  ) => {
+    const nonce = await marketplaceContract.getNonce(offerer.address);
+
+    const salt = !extraCheap ? randomHex() : constants.HashZero;
+    const startTime =
+      timeFlag !== "NOT_STARTED" ? 0 : toBN("0xee00000000000000000000000000");
+    const endTime =
+      timeFlag !== "EXPIRED" ? toBN("0xff00000000000000000000000000") : 1;
+
+    const orderParameters = {
+      offerer: offerer.address,
+      zone: !extraCheap ? (zone as Wallet).address : constants.AddressZero,
+      offer,
+      consideration,
+      totalOriginalConsiderationItems: consideration.length,
+      orderType,
+      zoneHash,
+      salt,
+      conduitKey,
+      startTime,
+      endTime,
+    };
+
+    const orderComponents = {
+      ...orderParameters,
+      nonce,
+    };
+
+    const orderHash = await getAndVerifyOrderHash(orderComponents);
+
+    const { isValidated, isCancelled, totalFilled, totalSize } =
+      await marketplaceContract.getOrderStatus(orderHash);
+
+    expect(isCancelled).to.equal(false);
+
+    const orderStatus = {
+      isValidated,
+      isCancelled,
+      totalFilled,
+      totalSize,
+    };
+
+    const flatSig = await signOrder(orderComponents, signer || offerer);
+
+    const order = {
+      parameters: orderParameters,
+      signature: !extraCheap ? flatSig : convertSignatureToEIP2098(flatSig),
+      numerator: 1, // only used for advanced orders
+      denominator: 1, // only used for advanced orders
+      extraData: "0x", // only used for advanced orders
+    };
+
+    // How much ether (at most) needs to be supplied when fulfilling the order
+    const value = offer
+      .map((x) =>
+        x.itemType === 0
+          ? x.endAmount.gt(x.startAmount)
+            ? x.endAmount
+            : x.startAmount
+          : toBN(0)
+      )
+      .reduce((a, b) => a.add(b), toBN(0))
+      .add(
+        consideration
+          .map((x) =>
+            x.itemType === 0
+              ? x.endAmount.gt(x.startAmount)
+                ? x.endAmount
+                : x.startAmount
+              : toBN(0)
+          )
+          .reduce((a, b) => a.add(b), toBN(0))
+      );
+
+    return {
+      order,
+      orderHash,
+      value,
+      orderStatus,
+      orderComponents,
+    };
+  };
+
+  const createMirrorBuyNowOrder = async (
+    offerer: Wallet,
+    zone: Wallet,
+    order: AdvancedOrder,
+    conduitKey = constants.HashZero
+  ) => {
+    const nonce = await marketplaceContract.getNonce(offerer.address);
+    const salt = randomHex();
+    const startTime = order.parameters.startTime;
+    const endTime = order.parameters.endTime;
+
+    const compressedOfferItems = [];
+    for (const {
+      itemType,
+      token,
+      identifierOrCriteria,
+      startAmount,
+      endAmount,
+    } of order.parameters.offer) {
+      if (
+        !compressedOfferItems
+          .map((x) => `${x.itemType}+${x.token}+${x.identifierOrCriteria}`)
+          .includes(`${itemType}+${token}+${identifierOrCriteria}`)
+      ) {
+        compressedOfferItems.push({
+          itemType,
+          token,
+          identifierOrCriteria,
+          startAmount: startAmount.eq(endAmount)
+            ? startAmount
+            : startAmount.sub(1),
+          endAmount: startAmount.eq(endAmount) ? endAmount : endAmount.sub(1),
+        });
+      } else {
+        const index = compressedOfferItems
+          .map((x) => `${x.itemType}+${x.token}+${x.identifierOrCriteria}`)
+          .indexOf(`${itemType}+${token}+${identifierOrCriteria}`);
+
+        compressedOfferItems[index].startAmount = compressedOfferItems[
+          index
+        ].startAmount.add(
+          startAmount.eq(endAmount) ? startAmount : startAmount.sub(1)
+        );
+        compressedOfferItems[index].endAmount = compressedOfferItems[
+          index
+        ].endAmount.add(
+          startAmount.eq(endAmount) ? endAmount : endAmount.sub(1)
+        );
+      }
+    }
+
+    const compressedConsiderationItems = [];
+    for (const {
+      itemType,
+      token,
+      identifierOrCriteria,
+      startAmount,
+      endAmount,
+      recipient,
+    } of order.parameters.consideration) {
+      if (
+        !compressedConsiderationItems
+          .map((x) => `${x.itemType}+${x.token}+${x.identifierOrCriteria}`)
+          .includes(`${itemType}+${token}+${identifierOrCriteria}`)
+      ) {
+        compressedConsiderationItems.push({
+          itemType,
+          token,
+          identifierOrCriteria,
+          startAmount: startAmount.eq(endAmount)
+            ? startAmount
+            : startAmount.add(1),
+          endAmount: startAmount.eq(endAmount) ? endAmount : endAmount.add(1),
+          recipient,
+        });
+      } else {
+        const index = compressedConsiderationItems
+          .map((x) => `${x.itemType}+${x.token}+${x.identifierOrCriteria}`)
+          .indexOf(`${itemType}+${token}+${identifierOrCriteria}`);
+
+        compressedConsiderationItems[index].startAmount =
+          compressedConsiderationItems[index].startAmount.add(
+            startAmount.eq(endAmount) ? startAmount : startAmount.add(1)
+          );
+        compressedConsiderationItems[index].endAmount =
+          compressedConsiderationItems[index].endAmount.add(
+            startAmount.eq(endAmount) ? endAmount : endAmount.add(1)
+          );
+      }
+    }
+
+    const orderParameters = {
+      offerer: offerer.address,
+      zone: zone.address,
+      offer: compressedConsiderationItems.map((x) => ({ ...x })),
+      consideration: compressedOfferItems.map((x) => ({
+        ...x,
+        recipient: offerer.address,
+      })),
+      totalOriginalConsiderationItems: compressedOfferItems.length,
+      orderType: order.parameters.orderType, // FULL_OPEN
+      zoneHash: "0x".padEnd(66, "0"),
+      salt,
+      conduitKey,
+      startTime,
+      endTime,
+    };
+
+    const orderComponents = {
+      ...orderParameters,
+      nonce,
+    };
+
+    const flatSig = await signOrder(orderComponents, offerer);
+
+    const mirrorOrderHash = await getAndVerifyOrderHash(orderComponents);
+
+    const mirrorOrder = {
+      parameters: orderParameters,
+      signature: flatSig,
+      numerator: order.numerator, // only used for advanced orders
+      denominator: order.denominator, // only used for advanced orders
+      extraData: "0x", // only used for advanced orders
+    };
+
+    // How much ether (at most) needs to be supplied when fulfilling the order
+    const mirrorValue = orderParameters.consideration
+      .map((x) =>
+        x.itemType === 0
+          ? x.endAmount.gt(x.startAmount)
+            ? x.endAmount
+            : x.startAmount
+          : toBN(0)
+      )
+      .reduce((a, b) => a.add(b), toBN(0));
+
+    return {
+      mirrorOrder,
+      mirrorOrderHash,
+      mirrorValue,
+    };
+  };
+
+  const createMirrorAcceptOfferOrder = async (
+    offerer: Wallet,
+    zone: Wallet,
+    order: AdvancedOrder,
+    criteriaResolvers: CriteriaResolver[] = [],
+    conduitKey = constants.HashZero
+  ) => {
+    const nonce = await marketplaceContract.getNonce(offerer.address);
+    const salt = randomHex();
+    const startTime = order.parameters.startTime;
+    const endTime = order.parameters.endTime;
+
+    const orderParameters = {
+      offerer: offerer.address,
+      zone: zone.address,
+      offer: order.parameters.consideration
+        .filter((x) => x.itemType !== 1)
+        .map((x) => ({
+          itemType: x.itemType < 4 ? x.itemType : x.itemType - 2,
+          token: x.token,
+          identifierOrCriteria:
+            x.itemType < 4
+              ? x.identifierOrCriteria
+              : criteriaResolvers[0].identifier,
+          startAmount: x.startAmount,
+          endAmount: x.endAmount,
+        })),
+      consideration: order.parameters.offer.map((x) => ({
+        itemType: x.itemType < 4 ? x.itemType : x.itemType - 2,
+        token: x.token,
+        identifierOrCriteria:
+          x.itemType < 4
+            ? x.identifierOrCriteria
+            : criteriaResolvers[0].identifier,
+        recipient: offerer.address,
+        startAmount: toBN(x.endAmount).sub(
+          order.parameters.consideration
+            .filter(
+              (i) =>
+                i.itemType < 2 &&
+                i.itemType === x.itemType &&
+                i.token === x.token
+            )
+            .map((i) => i.endAmount)
+            .reduce((a, b) => a.add(b), toBN(0))
+        ),
+        endAmount: toBN(x.endAmount).sub(
+          order.parameters.consideration
+            .filter(
+              (i) =>
+                i.itemType < 2 &&
+                i.itemType === x.itemType &&
+                i.token === x.token
+            )
+            .map((i) => i.endAmount)
+            .reduce((a, b) => a.add(b), toBN(0))
+        ),
+      })),
+      totalOriginalConsiderationItems: order.parameters.offer.length,
+      orderType: 0, // FULL_OPEN
+      zoneHash: constants.HashZero,
+      salt,
+      conduitKey,
+      startTime,
+      endTime,
+    };
+
+    const orderComponents = {
+      ...orderParameters,
+      nonce,
+    };
+
+    const flatSig = await signOrder(orderComponents as any, offerer);
+
+    const mirrorOrderHash = await getAndVerifyOrderHash(orderComponents as any);
+
+    const mirrorOrder = {
+      parameters: orderParameters,
+      signature: flatSig,
+      numerator: 1, // only used for advanced orders
+      denominator: 1, // only used for advanced orders
+      extraData: "0x", // only used for advanced orders
+    };
+
+    // How much ether (at most) needs to be supplied when fulfilling the order
+    const mirrorValue = orderParameters.consideration
+      .map((x) =>
+        x.itemType === 0
+          ? x.endAmount.gt(x.startAmount)
+            ? x.endAmount
+            : x.startAmount
+          : toBN(0)
+      )
+      .reduce((a, b) => a.add(b), toBN(0));
+
+    return {
+      mirrorOrder,
+      mirrorOrderHash,
+      mirrorValue,
+    };
+  };
+
+  return {
+    marketplaceContract,
+    directMarketplaceContract,
+    stubZone,
+    domainData,
+    signOrder,
+    createOrder,
+    createMirrorBuyNowOrder,
+    createMirrorAcceptOfferOrder,
+  };
+}
---
+++ test/utils/fixtures/tokens.ts
@@ -0,0 +1,301 @@
+/* eslint-disable camelcase */
+import { JsonRpcSigner } from "@ethersproject/providers";
+import { expect } from "chai";
+import { BigNumber, constants, Wallet } from "ethers";
+import { ethers } from "hardhat";
+import { TestERC1155, TestERC20, TestERC721 } from "../../../typechain-types";
+import { deployContract } from "../contracts";
+import {
+  randomBN,
+  toBN,
+  BigNumberish,
+  getOfferOrConsiderationItem,
+  random128,
+} from "../encoding";
+import { whileImpersonating } from "../impersonate";
+
+export const fixtureERC20 = async (signer: JsonRpcSigner) => {
+  const testERC20: TestERC20 = await deployContract("TestERC20", signer);
+
+  const mintAndApproveERC20 = async (
+    signer: Wallet,
+    spender: string,
+    tokenAmount: BigNumberish
+  ) => {
+    const amount = toBN(tokenAmount);
+    // Offerer mints ERC20
+    await testERC20.mint(signer.address, amount);
+
+    // Offerer approves marketplace contract to tokens
+    await expect(testERC20.connect(signer).approve(spender, amount))
+      .to.emit(testERC20, "Approval")
+      .withArgs(signer.address, spender, tokenAmount);
+  };
+
+  const getTestItem20 = (
+    startAmount: BigNumberish = 50,
+    endAmount: BigNumberish = 50,
+    recipient: string,
+    token = testERC20.address
+  ) =>
+    getOfferOrConsiderationItem(1, token, 0, startAmount, endAmount, recipient);
+
+  return {
+    testERC20,
+    mintAndApproveERC20,
+    getTestItem20,
+  };
+};
+
+export const fixtureERC721 = async (signer: JsonRpcSigner) => {
+  const testERC721: TestERC721 = await deployContract("TestERC721", signer);
+
+  const set721ApprovalForAll = (
+    signer: Wallet,
+    spender: string,
+    approved = true,
+    contract = testERC721
+  ) => {
+    return expect(contract.connect(signer).setApprovalForAll(spender, approved))
+      .to.emit(contract, "ApprovalForAll")
+      .withArgs(signer.address, spender, approved);
+  };
+
+  const mint721 = async (signer: Wallet, id?: BigNumberish) => {
+    const nftId = id ? toBN(id) : randomBN();
+    await testERC721.mint(signer.address, nftId);
+    return nftId;
+  };
+
+  const mint721s = async (signer: Wallet, count: number) => {
+    const arr = [];
+    for (let i = 0; i < count; i++) arr.push(await mint721(signer));
+    return arr;
+  };
+
+  const mintAndApprove721 = async (
+    signer: Wallet,
+    spender: string,
+    id?: BigNumberish
+  ) => {
+    await set721ApprovalForAll(signer, spender, true);
+    return mint721(signer, id);
+  };
+
+  const getTestItem721 = (
+    identifierOrCriteria: BigNumberish,
+    startAmount: BigNumberish = 1,
+    endAmount: BigNumberish = 1,
+    recipient: string,
+    token = testERC721.address
+  ) =>
+    getOfferOrConsiderationItem(
+      2,
+      token,
+      identifierOrCriteria,
+      startAmount,
+      endAmount,
+      recipient
+    );
+
+  const getTestItem721WithCriteria = (
+    identifierOrCriteria: BigNumberish,
+    startAmount: BigNumberish = 1,
+    endAmount: BigNumberish = 1,
+    recipient: string
+  ) =>
+    getOfferOrConsiderationItem(
+      4,
+      testERC721.address,
+      identifierOrCriteria,
+      startAmount,
+      endAmount,
+      recipient
+    );
+
+  return {
+    testERC721,
+    set721ApprovalForAll,
+    mint721,
+    mint721s,
+    mintAndApprove721,
+    getTestItem721,
+    getTestItem721WithCriteria,
+  };
+};
+
+export const fixtureERC1155 = async (signer: JsonRpcSigner) => {
+  const testERC1155: TestERC1155 = await deployContract("TestERC1155", signer);
+
+  const set1155ApprovalForAll = (
+    signer: Wallet,
+    spender: string,
+    approved = true,
+    token = testERC1155
+  ) => {
+    return expect(token.connect(signer).setApprovalForAll(spender, approved))
+      .to.emit(token, "ApprovalForAll")
+      .withArgs(signer.address, spender, approved);
+  };
+
+  const mint1155 = async (
+    signer: Wallet,
+    multiplier = 1,
+    token = testERC1155,
+    id?: BigNumberish,
+    amt?: BigNumberish
+  ) => {
+    const nftId = id ? toBN(id) : randomBN();
+    const amount = amt ? toBN(amt) : toBN(randomBN(4));
+    await token.mint(signer.address, nftId, amount.mul(multiplier));
+    return { nftId, amount };
+  };
+
+  const mintAndApprove1155 = async (
+    signer: Wallet,
+    spender: string,
+    multiplier = 1,
+    id?: BigNumberish,
+    amt?: BigNumberish
+  ) => {
+    const { nftId, amount } = await mint1155(
+      signer,
+      multiplier,
+      testERC1155,
+      id,
+      amt
+    );
+    await set1155ApprovalForAll(signer, spender, true);
+    return { nftId, amount };
+  };
+
+  const getTestItem1155WithCriteria = (
+    identifierOrCriteria: BigNumberish,
+    startAmount: BigNumberish = 1,
+    endAmount: BigNumberish = 1,
+    recipient: string
+  ) =>
+    getOfferOrConsiderationItem(
+      5,
+      testERC1155.address,
+      identifierOrCriteria,
+      startAmount,
+      endAmount,
+      recipient
+    );
+
+  const getTestItem1155 = (
+    identifierOrCriteria: BigNumberish,
+    startAmount: BigNumberish,
+    endAmount: BigNumberish,
+    token = testERC1155.address,
+    recipient: string
+  ) =>
+    getOfferOrConsiderationItem(
+      3,
+      token,
+      identifierOrCriteria,
+      startAmount,
+      endAmount,
+      recipient
+    );
+
+  return {
+    testERC1155,
+    set1155ApprovalForAll,
+    mint1155,
+    mintAndApprove1155,
+    getTestItem1155WithCriteria,
+    getTestItem1155,
+  };
+};
+
+const minRandom = (min: number) => randomBN(10).add(min);
+
+export const tokensFixture = async (signer: JsonRpcSigner) => {
+  const erc20 = await fixtureERC20(signer);
+  const erc721 = await fixtureERC721(signer);
+  const erc1155 = await fixtureERC1155(signer);
+  const { testERC1155: testERC1155Two } = await fixtureERC1155(signer);
+  const tokenByType = [
+    {
+      address: constants.AddressZero,
+    } as any, // ETH
+    erc20.testERC20,
+    erc721.testERC721,
+    erc1155.testERC1155,
+  ];
+  const createTransferWithApproval = async (
+    contract: TestERC20 | TestERC1155 | TestERC721,
+    receiver: Wallet,
+    itemType: 0 | 1 | 2 | 3 | 4 | 5,
+    approvalAddress: string,
+    from: string,
+    to: string
+  ) => {
+    let identifier: BigNumber = toBN(0);
+    let amount: BigNumber = toBN(0);
+    const token = contract.address;
+
+    switch (itemType) {
+      case 0:
+        break;
+      case 1: // ERC20
+        amount = minRandom(100);
+        await (contract as TestERC20).mint(receiver.address, amount);
+
+        // Receiver approves contract to transfer tokens
+        await whileImpersonating(
+          receiver.address,
+          ethers.provider,
+          async () => {
+            await expect(
+              (contract as TestERC20)
+                .connect(receiver)
+                .approve(approvalAddress, amount)
+            )
+              .to.emit(contract, "Approval")
+              .withArgs(receiver.address, approvalAddress, amount);
+          }
+        );
+        break;
+      case 2: // ERC721
+      case 4: // ERC721_WITH_CRITERIA
+        amount = toBN(1);
+        identifier = randomBN();
+        await (contract as TestERC721).mint(receiver.address, identifier);
+
+        // Receiver approves contract to transfer tokens
+        await erc721.set721ApprovalForAll(
+          receiver,
+          approvalAddress,
+          true,
+          contract as TestERC721
+        );
+        break;
+      case 3: // ERC1155
+      case 5: // ERC1155_WITH_CRITERIA
+        identifier = random128();
+        amount = minRandom(1);
+        await contract.mint(receiver.address, identifier, amount);
+
+        // Receiver approves contract to transfer tokens
+        await erc1155.set1155ApprovalForAll(
+          receiver,
+          approvalAddress,
+          true,
+          contract as TestERC1155
+        );
+        break;
+    }
+    return { itemType, token, from, to, identifier, amount };
+  };
+  return {
+    ...erc20,
+    ...erc721,
+    ...erc1155,
+    testERC1155Two,
+    tokenByType,
+    createTransferWithApproval,
+  };
+};
