c38d7f069803398432371201d08ed3cf216e8256
eugenioclrc <eugenioclrc@gmail.com>
2022-06-06 13:00:45 -0300

    Enforce code style, pre increment and pre decrement

--- reference/lib/ReferenceOrderCombiner.sol
+++ reference/lib/ReferenceOrderCombiner.sol
@@ -482,7 +482,7 @@ contract ReferenceOrderCombiner is
             // If offerer and recipient on the execution are the same...
             if (execution.item.recipient == execution.offerer) {
                 // increment total filtered executions.
-                totalFilteredExecutions += 1;
+                ++totalFilteredExecutions;
             } else {
                 // Otherwise, assign the execution to the executions array.
                 executions[i - totalFilteredExecutions] = execution;
@@ -508,7 +508,7 @@ contract ReferenceOrderCombiner is
             // If offerer and recipient on the execution are the same...
             if (execution.item.recipient == execution.offerer) {
                 // increment total filtered executions.
-                totalFilteredExecutions += 1;
+                ++totalFilteredExecutions;
             } else {
                 // Otherwise, assign the execution to the executions array.
                 executions[
@@ -765,7 +765,7 @@ contract ReferenceOrderCombiner is
             // If offerer and recipient on the execution are the same...
             if (execution.item.recipient == execution.offerer) {
                 // increment total filtered executions.
-                totalFilteredExecutions += 1;
+                ++totalFilteredExecutions;
             } else {
                 // Otherwise, assign the execution to the executions array.
                 executions[i - totalFilteredExecutions] = execution;
