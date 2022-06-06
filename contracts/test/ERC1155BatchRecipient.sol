f8945e3cc1fba9ae67ef57844f6df1fa1d61730b
d1ll0n <dillonkellar@protonmail.com>
2022-06-06 07:16:14 -0500

    Add tests for conduit ERC1155 batch transfer reverts

---
+++ contracts/test/ERC1155BatchRecipient.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.7;
+
+contract ERC1155BatchRecipient {
+    error UnexpectedBatchData();
+
+    function onERC1155BatchReceived(
+        address,
+        address,
+        uint256[] calldata,
+        uint256[] calldata,
+        bytes memory data
+    ) external view returns (bytes4) {
+        if (data.length != 0) {
+            revert UnexpectedBatchData();
+        }
+        return ERC1155BatchRecipient.onERC1155BatchReceived.selector;
+    }
+}
