0410f66f6586fca76b7b07c7f6fd8248ff9e3b12
Chomtana <Chomtana001@gmail.com>
2022-06-07 14:25:38 +0700

    Add more credit inplace of @hrkrshnn + @axic

--- contracts/lib/AmountDeriver.sol
+++ contracts/lib/AmountDeriver.sol
@@ -103,6 +103,7 @@ contract AmountDeriver is AmountDerivationErrors {
         assembly {
             // Ensure new value contains no remainder via mulmod operator.
             // Credit to @hrkrshnn + @axic for proposing this optimal solution.
+            // Further optimization by @Chomtana
             if gt(mulmod(value, numerator, denominator), 0) {
                 mstore(0, InexactFraction_error_signature)
                 revert(0, InexactFraction_error_len)
