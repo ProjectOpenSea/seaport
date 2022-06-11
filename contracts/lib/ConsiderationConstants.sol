86e7ec6c871d8ecec93d6f038ae9eda3065a86ae
0age <0age@protonmail.com>
2022-06-10 17:21:44 -0700

    implement nits from readthrough

--- contracts/lib/ConsiderationConstants.sol
+++ contracts/lib/ConsiderationConstants.sol
@@ -100,7 +100,6 @@ uint256 constant OneWord = 0x20;
 uint256 constant TwoWords = 0x40;
 uint256 constant ThreeWords = 0x60;
 uint256 constant FourWords = 0x80;
-uint256 constant FiveWords = 0xa0;
 
 uint256 constant FreeMemoryPointerSlot = 0x40;
 uint256 constant ZeroSlot = 0x60;
@@ -110,7 +109,7 @@ uint256 constant Slot0x80 = 0x80;
 uint256 constant Slot0xA0 = 0xa0;
 
 uint256 constant BasicOrder_endAmount_cdPtr = 0x104;
-
+uint256 constant BasicOrder_common_params_size = 0xa0;
 uint256 constant BasicOrder_considerationHashesArray_ptr = 0x160;
 
 uint256 constant EIP712_Order_size = 0x180;
