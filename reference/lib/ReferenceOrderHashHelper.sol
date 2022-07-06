50692fa25fc56d04add308faffb47673cfe2029a
Steven Barsam <steven@s5stratos.com>
2022-07-06 17:47:23 -0500

    Change pragma solidity versions to use carets. Remove prettier

--- reference/lib/ReferenceOrderHashHelper.sol
+++ reference/lib/ReferenceOrderHashHelper.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.7;
+pragma solidity ^0.8.7;
 
 import { ConsiderationItem, OfferItem, OrderParameters } from "../../contracts/lib/ConsiderationStructs.sol";
 
