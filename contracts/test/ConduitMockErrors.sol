c0e25e1f6e49b38b29f00b64f747215be5320e3a
stephankmin <stephankmin@gmail.com>
2022-07-12 18:20:26 -0700

    remove unused mock conduit

--- contracts/test/ConduitMockErrors.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
-
-interface ConduitMockErrors {
-    error RevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevertRevert();
-}
