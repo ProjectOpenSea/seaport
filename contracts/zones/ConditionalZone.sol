3fbe554a2e446274ef2cb8d64f12c24c870f9204
Kartik <slokh@octane.gg>
2022-08-21 14:10:41 -0400

    Support 6 main logic gates

--- contracts/zones/ConditionalZone.sol
+++ contracts/zones/ConditionalZone.sol
@@ -9,16 +9,27 @@ import {
     CriteriaResolver
 } from "../lib/ConsiderationStructs.sol";
 
-error PermanentlyInvalid();
-error TemporarilyInvalid();
-
 /**
  * @title  ConditionalZone
  * @author Slokh
- * @notice ConditionalZone allows for IFTTT types of orders.
+ * @notice ConditionalZone allows for orders validated by logic gates.
  */
 contract ConditionalZone is ZoneInterface {
     SeaportInterface seaport;
+
+    enum LogicGate {
+        AND,
+        OR,
+        XOR,
+        NAND,
+        NOR,
+        XNOR
+    }
+
+    struct Condition {
+        LogicGate logicGate;
+        bytes32[] orderHashes;
+    }
 
     constructor(address seaportAddress) {
         seaport = SeaportInterface(seaportAddress);
@@ -43,29 +54,15 @@ contract ConditionalZone is ZoneInterface {
         address caller,
         address offerer,
         bytes32 zoneHash
-    ) external view override returns (bytes4 validOrderMagicValue) {
+    ) external pure override returns (bytes4 validOrderMagicValue) {
         orderHash;
         caller;
         offerer;
+        zoneHash;
 
-        // Only validate if there is a zoneHash
+        // If the order has a zoneHash, extraData must be provided to be validated against.
         if (zoneHash != "") {
-            (
-                ,
-                bool isCancelled,
-                uint256 totalFilled,
-                uint256 totalSize
-            ) = seaport.getOrderStatus(zoneHash);
-
-            // If the dependant order was cancelled, this order can never be valid.
-            if (isCancelled) {
-                return 0x0;
-            }
-
-            // If the dependant order has not been filled, this order is not valid yet.
-            if (totalFilled == 0 || totalFilled != totalSize) {
-                return 0x0;
-            }
+            revert("Must provide extraData");
         }
 
         // Return the selector of isValidOrder as the magic value.
@@ -96,14 +93,109 @@ contract ConditionalZone is ZoneInterface {
         AdvancedOrder calldata order,
         bytes32[] calldata priorOrderHashes,
         CriteriaResolver[] calldata criteriaResolvers
-    ) external pure override returns (bytes4 validOrderMagicValue) {
+    ) external view override returns (bytes4 validOrderMagicValue) {
         orderHash;
         caller;
-        order;
         priorOrderHashes;
         criteriaResolvers;
 
+        // Only validate if there is a zoneHash
+        if (order.parameters.zoneHash != "") {
+            if (order.parameters.zoneHash != keccak256(order.extraData)) {
+                revert("Hash does not match");
+            }
+
+            Condition memory condition = decodeCondition(order.extraData);
+
+            // TODO: Optimize this entire loop
+            uint256 conditionResult;
+            for (uint256 i = 0; i < condition.orderHashes.length; i++) {
+                (
+                    ,
+                    bool isCancelled,
+                    uint256 totalFilled,
+                    uint256 totalSize
+                ) = seaport.getOrderStatus(condition.orderHashes[i]);
+
+                // If the an order in the condition is cancelled, this order can never be valid.
+                if (isCancelled) {
+                    revert("A dependant order is cancelled");
+                }
+
+                uint256 isFilled = totalFilled > 0 && totalFilled == totalSize
+                    ? 1
+                    : 0;
+                if (i == 0) {
+                    conditionResult = isFilled;
+                } else if (
+                    condition.logicGate == LogicGate.AND ||
+                    condition.logicGate == LogicGate.NAND
+                ) {
+                    conditionResult = conditionResult & isFilled;
+                } else if (
+                    condition.logicGate == LogicGate.OR ||
+                    condition.logicGate == LogicGate.NOR
+                ) {
+                    conditionResult = conditionResult | isFilled;
+                } else if (
+                    condition.logicGate == LogicGate.XOR ||
+                    condition.logicGate == LogicGate.XNOR
+                ) {
+                    conditionResult = conditionResult ^ isFilled;
+                }
+            }
+
+            uint256 expectedResult = condition.logicGate == LogicGate.NAND ||
+                condition.logicGate == LogicGate.NOR ||
+                condition.logicGate == LogicGate.XNOR
+                ? 0
+                : 1;
+
+            if (conditionResult != expectedResult) {
+                revert("Condition not met");
+            }
+        }
+
         // Return the selector of isValidOrder as the magic value.
         validOrderMagicValue = ZoneInterface.isValidOrder.selector;
+    }
+
+    function decodeCondition(bytes memory _data)
+        public
+        pure
+        returns (Condition memory condition)
+    {
+        uint256 dataLength = _data.length;
+
+        uint8 tempLogicGate;
+        assembly {
+            tempLogicGate := mload(add(_data, 1))
+        }
+        condition.logicGate = LogicGate(tempLogicGate);
+
+        bytes32[] memory orderHashes = new bytes32[](dataLength / 32);
+        uint256 index = 0;
+        bytes32 temp;
+
+        for (uint256 i = 33; i <= dataLength; i += 32) {
+            assembly {
+                temp := mload(add(_data, i))
+            }
+            orderHashes[index] = temp;
+            index++;
+        }
+
+        condition.orderHashes = orderHashes;
+    }
+
+    function encodeCondition(Condition memory condition)
+        public
+        pure
+        returns (bytes memory data)
+    {
+        data = abi.encodePacked(
+            uint8(condition.logicGate),
+            condition.orderHashes
+        );
     }
 }
--- test/conditionals.spec.ts
+++ test/conditionals.spec.ts
@@ -1,4 +1,5 @@
 import { expect } from "chai";
+import { keccak256 } from "ethers/lib/utils";
 import { ethers, network } from "hardhat";
 
 import { getItemETH, randomHex, toKey } from "./utils/encoding";
@@ -7,6 +8,7 @@ import { seaportFixture } from "./utils/fixtures";
 import { VERSION } from "./utils/helpers";
 
 import type {
+  ConditionalZone,
   ConsiderationInterface,
   EIP1271Wallet,
   EIP1271Wallet__factory,
@@ -79,30 +81,85 @@ describe(`Zone - ConditionalZone (Seaport v${VERSION})`, function () {
     return zoneContract;
   }
 
-  it("Fulfill an order through zone with no conditional", async () => {
-    const zone = await createZone(marketplaceContract.address);
-
+  async function createOrderWithZone(
+    zone: ConditionalZone,
+    condition?: ConditionalZone.ConditionStruct
+  ) {
     const nftId = await mintAndApprove721(seller, marketplaceContract.address);
 
-    const offer = [getTestItem721(nftId)];
+    let zoneHash;
+    let conditionData;
 
-    const consideration = [
-      getItemETH(parseEther("10"), parseEther("10"), seller.address),
-      getItemETH(parseEther("1"), parseEther("1"), owner.address),
-    ];
+    if (condition) {
+      conditionData = await zone.encodeCondition(condition);
+      zoneHash = keccak256(conditionData);
+    }
 
-    const { order, orderHash, value } = await createOrder(
+    const order = await createOrder(
       seller,
       zone.address,
-      offer,
-      consideration,
-      2 // FULL_RESTRICTED
+      // offer
+      [getTestItem721(nftId)],
+      // consideration
+      [
+        getItemETH(parseEther("10"), parseEther("10"), seller.address),
+        getItemETH(parseEther("1"), parseEther("1"), owner.address),
+      ],
+      2,
+      undefined,
+      undefined,
+      undefined,
+      zoneHash
     );
+    if (conditionData) {
+      order.order.extraData = conditionData;
+    }
+
+    return order;
+  }
+
+  async function fulfillAdvancedOrder({ order, orderHash, value }: any) {
+    await withBalanceChecks([order], 0, undefined, async () => {
+      const tx = await marketplaceContract
+        .connect(buyer)
+        .fulfillAdvancedOrder(order, [], toKey(0), buyer.address, {
+          value,
+        });
+
+      const receipt = await tx.wait();
+      await checkExpectedEvents(tx, receipt, [
+        {
+          order,
+          orderHash,
+          fulfiller: buyer.address,
+          fulfillerConduitKey: toKey(0),
+        },
+      ]);
+      return receipt;
+    });
+  }
+
+  async function assertFulfillAdvancedOrderReverts(
+    { order, value }: any,
+    revertMessage: string
+  ) {
+    await expect(
+      marketplaceContract
+        .connect(buyer)
+        .fulfillAdvancedOrder(order, [], toKey(0), buyer.address, {
+          value,
+        })
+    ).to.be.revertedWith(revertMessage);
+  }
+
+  it("Fulfill an order through zone with no condition", async () => {
+    const zone = await createZone(marketplaceContract.address);
+    const { order, orderHash, value } = await createOrderWithZone(zone);
 
     await withBalanceChecks([order], 0, undefined, async () => {
       const tx = await marketplaceContract
         .connect(buyer)
-        .fulfillOrder(order, toKey(0), {
+        .fulfillAdvancedOrder(order, [], toKey(0), buyer.address, {
           value,
         });
 
@@ -119,132 +176,230 @@ describe(`Zone - ConditionalZone (Seaport v${VERSION})`, function () {
     });
   });
 
-  it("Fulfill an order through zone dependant on another order", async () => {
-    const zone = await createZone(marketplaceContract.address);
-
-    const nftId = await mintAndApprove721(seller, marketplaceContract.address);
-    const nftId2 = await mintAndApprove721(seller, marketplaceContract.address);
-
-    const firstOrder = await createOrder(
-      seller,
-      zone.address,
-      // offer
-      [getTestItem721(nftId)],
-      // consideration
-      [
-        getItemETH(parseEther("10"), parseEther("10"), seller.address),
-        getItemETH(parseEther("1"), parseEther("1"), owner.address),
-      ],
-      2
-    );
-
-    const secondOrder = await createOrder(
-      seller,
-      zone.address,
-      // offer
-      [getTestItem721(nftId2)],
-      // consideration
-      [
-        getItemETH(parseEther("10"), parseEther("10"), seller.address),
-        getItemETH(parseEther("1"), parseEther("1"), owner.address),
-      ],
-      2,
-      undefined,
-      undefined,
-      undefined,
-      firstOrder.orderHash
-    );
-
-    // Second order is not valid
-    await expect(
-      marketplaceContract
-        .connect(buyer)
-        .fulfillOrder(secondOrder.order, toKey(0), {
-          value: secondOrder.value,
-        })
-    ).to.be.revertedWith(`InvalidRestrictedOrder("${secondOrder.orderHash}")`);
-
-    const fulfillOrder = async ({ order, orderHash, value }: any) => {
-      await withBalanceChecks([order], 0, undefined, async () => {
-        const tx = await marketplaceContract
-          .connect(buyer)
-          .fulfillOrder(order, toKey(0), {
-            value,
-          });
-
-        const receipt = await tx.wait();
-        await checkExpectedEvents(tx, receipt, [
-          {
-            order,
-            orderHash,
-            fulfiller: buyer.address,
-            fulfillerConduitKey: toKey(0),
-          },
-        ]);
-        return receipt;
-      });
-    };
-
-    await fulfillOrder(firstOrder);
-    await fulfillOrder(secondOrder);
-  });
-
   it("Cannot fulfill an order when dependant order is cancelled", async () => {
     const zone = await createZone(marketplaceContract.address);
-
-    const nftId = await mintAndApprove721(seller, marketplaceContract.address);
-    const nftId2 = await mintAndApprove721(seller, marketplaceContract.address);
-
-    const firstOrder = await createOrder(
-      seller,
-      zone.address,
-      // offer
-      [getTestItem721(nftId)],
-      // consideration
-      [
-        getItemETH(parseEther("10"), parseEther("10"), seller.address),
-        getItemETH(parseEther("1"), parseEther("1"), owner.address),
-      ],
-      2
-    );
-
-    const secondOrder = await createOrder(
-      seller,
-      zone.address,
-      // offer
-      [getTestItem721(nftId2)],
-      // consideration
-      [
-        getItemETH(parseEther("10"), parseEther("10"), seller.address),
-        getItemETH(parseEther("1"), parseEther("1"), owner.address),
-      ],
-      2,
-      undefined,
-      undefined,
-      undefined,
-      firstOrder.orderHash
-    );
+    const firstOrder = await createOrderWithZone(zone);
+    const secondOrder = await createOrderWithZone(zone, {
+      logicGate: 0,
+      orderHashes: [firstOrder.orderHash],
+    });
 
     await marketplaceContract
       .connect(seller)
       .cancel([firstOrder.orderComponents]);
 
-    // First order is not valid
-    await expect(
-      marketplaceContract
-        .connect(buyer)
-        .fulfillOrder(firstOrder.order, toKey(0), {
-          value: firstOrder.value,
-        })
-    ).to.be.revertedWith(`OrderIsCancelled("${firstOrder.orderHash}")`);
+    await assertFulfillAdvancedOrderReverts(
+      firstOrder,
+      `OrderIsCancelled("${firstOrder.orderHash}")`
+    );
 
-    // Second order is not valid
-    await expect(
-      marketplaceContract
-        .connect(buyer)
-        .fulfillOrder(secondOrder.order, toKey(0), {
-          value: secondOrder.value,
-        })
-    ).to.be.revertedWith(`InvalidRestrictedOrder("${secondOrder.orderHash}")`);
+    await assertFulfillAdvancedOrderReverts(
+      secondOrder,
+      "A dependant order is cancelled"
+    );
+  });
+
+  it("Must provide extraData when zoneHash exists", async () => {
+    const zone = await createZone(marketplaceContract.address);
+    const firstOrder = await createOrderWithZone(zone);
+    const secondOrder = await createOrderWithZone(zone, {
+      logicGate: 0,
+      orderHashes: [firstOrder.orderHash],
+    });
+
+    // Reset extraData
+    secondOrder.order.extraData = "0x";
+
+    await assertFulfillAdvancedOrderReverts(
+      secondOrder,
+      "Must provide extraData"
+    );
+  });
+
+  it("zoneHash must match hash of extraData", async () => {
+    const zone = await createZone(marketplaceContract.address);
+    const firstOrder = await createOrderWithZone(zone);
+    const secondOrder = await createOrderWithZone(zone, {
+      logicGate: 0,
+      orderHashes: [firstOrder.orderHash],
+    });
+
+    // Reset extraData
+    secondOrder.order.extraData = firstOrder.orderHash;
+
+    await assertFulfillAdvancedOrderReverts(secondOrder, "Hash does not match");
+  });
+
+  it("Fulfill an order with condition - AND", async () => {
+    const zone = await createZone(marketplaceContract.address);
+    const firstOrder = await createOrderWithZone(zone);
+    const secondOrder = await createOrderWithZone(zone);
+    const thirdOrder = await createOrderWithZone(zone, {
+      logicGate: 0,
+      orderHashes: [firstOrder.orderHash, secondOrder.orderHash],
+    });
+
+    // Third order is not valid
+    await assertFulfillAdvancedOrderReverts(thirdOrder, "Condition not met");
+
+    // Fulfill first order
+    await fulfillAdvancedOrder(firstOrder);
+
+    // Third order is still not valid
+    await assertFulfillAdvancedOrderReverts(thirdOrder, "Condition not met");
+
+    // Fulfill second order
+    await fulfillAdvancedOrder(secondOrder);
+
+    // Third order is valid
+    await fulfillAdvancedOrder(thirdOrder);
+  });
+
+  it("Fulfill an order with condition - OR", async () => {
+    const zone = await createZone(marketplaceContract.address);
+    const firstOrder = await createOrderWithZone(zone);
+    const secondOrder = await createOrderWithZone(zone);
+    const thirdOrder = await createOrderWithZone(zone, {
+      logicGate: 1,
+      orderHashes: [firstOrder.orderHash, secondOrder.orderHash],
+    });
+    const fourthOrder = await createOrderWithZone(zone, {
+      logicGate: 1,
+      orderHashes: [firstOrder.orderHash, secondOrder.orderHash],
+    });
+
+    // Third order is not valid
+    await assertFulfillAdvancedOrderReverts(thirdOrder, "Condition not met");
+
+    // Fourth order is not valid
+    await assertFulfillAdvancedOrderReverts(fourthOrder, "Condition not met");
+
+    // Fulfill first order
+    await fulfillAdvancedOrder(firstOrder);
+
+    // Third order is valid
+    await fulfillAdvancedOrder(thirdOrder);
+
+    // Fulfill second order
+    await fulfillAdvancedOrder(secondOrder);
+
+    // Fourth order is valid
+    await fulfillAdvancedOrder(fourthOrder);
+  });
+
+  it("Fulfill an order with condition - XOR", async () => {
+    const zone = await createZone(marketplaceContract.address);
+    const firstOrder = await createOrderWithZone(zone);
+    const secondOrder = await createOrderWithZone(zone);
+    const thirdOrder = await createOrderWithZone(zone, {
+      logicGate: 2,
+      orderHashes: [firstOrder.orderHash, secondOrder.orderHash],
+    });
+    const fourthOrder = await createOrderWithZone(zone, {
+      logicGate: 2,
+      orderHashes: [firstOrder.orderHash, secondOrder.orderHash],
+    });
+
+    // Third order is not valid
+    await assertFulfillAdvancedOrderReverts(thirdOrder, "Condition not met");
+
+    // Fourth order is not valid
+    await assertFulfillAdvancedOrderReverts(fourthOrder, "Condition not met");
+
+    // Fulfill first order
+    await fulfillAdvancedOrder(firstOrder);
+
+    // Third order is valid
+    await fulfillAdvancedOrder(thirdOrder);
+
+    // Fulfill second order
+    await fulfillAdvancedOrder(secondOrder);
+
+    // Fourth order is not valid
+    await assertFulfillAdvancedOrderReverts(fourthOrder, "Condition not met");
+  });
+
+  it("Fulfill an order with condition - NAND", async () => {
+    const zone = await createZone(marketplaceContract.address);
+    const firstOrder = await createOrderWithZone(zone);
+    const secondOrder = await createOrderWithZone(zone);
+    const thirdOrder = await createOrderWithZone(zone, {
+      logicGate: 3,
+      orderHashes: [firstOrder.orderHash, secondOrder.orderHash],
+    });
+    const fourthOrder = await createOrderWithZone(zone, {
+      logicGate: 3,
+      orderHashes: [firstOrder.orderHash, secondOrder.orderHash],
+    });
+
+    // Third order is valid
+    await fulfillAdvancedOrder(thirdOrder);
+
+    // Fulfill first order
+    await fulfillAdvancedOrder(firstOrder);
+
+    // Fulfill second order
+    await fulfillAdvancedOrder(secondOrder);
+
+    // Fourth order is not valid
+    await assertFulfillAdvancedOrderReverts(fourthOrder, "Condition not met");
+  });
+
+  it("Fulfill an order with condition - NOR", async () => {
+    const zone = await createZone(marketplaceContract.address);
+    const firstOrder = await createOrderWithZone(zone);
+    const secondOrder = await createOrderWithZone(zone);
+    const thirdOrder = await createOrderWithZone(zone, {
+      logicGate: 4,
+      orderHashes: [firstOrder.orderHash, secondOrder.orderHash],
+    });
+    const fourthOrder = await createOrderWithZone(zone, {
+      logicGate: 4,
+      orderHashes: [firstOrder.orderHash, secondOrder.orderHash],
+    });
+
+    // Third order is valid
+    await fulfillAdvancedOrder(thirdOrder);
+
+    // Fulfill first order
+    await fulfillAdvancedOrder(firstOrder);
+
+    // Fourth order is not valid
+    await assertFulfillAdvancedOrderReverts(fourthOrder, "Condition not met");
+
+    // Fulfill second order
+    await fulfillAdvancedOrder(secondOrder);
+
+    // Fourth order is not valid
+    await assertFulfillAdvancedOrderReverts(fourthOrder, "Condition not met");
+  });
+
+  it("Fulfill an order with condition - XNOR", async () => {
+    const zone = await createZone(marketplaceContract.address);
+    const firstOrder = await createOrderWithZone(zone);
+    const secondOrder = await createOrderWithZone(zone);
+    const thirdOrder = await createOrderWithZone(zone, {
+      logicGate: 5,
+      orderHashes: [firstOrder.orderHash, secondOrder.orderHash],
+    });
+    const fourthOrder = await createOrderWithZone(zone, {
+      logicGate: 5,
+      orderHashes: [firstOrder.orderHash, secondOrder.orderHash],
+    });
+
+    // Third order is valid
+    await fulfillAdvancedOrder(thirdOrder);
+
+    // Fulfill first order
+    await fulfillAdvancedOrder(firstOrder);
+
+    // Fourth order is not valid
+    await assertFulfillAdvancedOrderReverts(fourthOrder, "Condition not met");
+
+    // Fulfill second order
+    await fulfillAdvancedOrder(secondOrder);
+
+    // Fulfill fourth order
+    await fulfillAdvancedOrder(fourthOrder);
   });
 });
