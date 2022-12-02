6bb5768b699d57db8eb1f763c0f45611979f3b72
Stephan Min <stephan.min@opensea.io>
2022-12-02 14:46:57 -0500

    add second test

--- test/advanced.spec.ts
+++ test/advanced.spec.ts
@@ -1594,6 +1594,102 @@ describe(`Advanced orders (Seaport v${VERSION})`, function () {
           })
       ).to.be.reverted;
     });
+    it("Reverts on contract orders where call to generateOrders throws and reverts are skipped", async () => {
+      // Seller mints nfts
+      const { nftId, amount } = await mintAndApprove1155(
+        seller,
+        marketplaceContract.address,
+        10000
+      );
+
+      // seller deploys offererContract with invalid generateOrders function
+      // and approves it for 1155 token
+      const offererContract = await deployContract(
+        "TestInvalidContractOfferer",
+        owner,
+        marketplaceContract.address
+      );
+
+      await set1155ApprovalForAll(seller, offererContract.address, true);
+
+      const offer = [
+        getTestItem1155(nftId, amount.mul(10), amount.mul(10)) as any,
+      ];
+
+      const consideration = [
+        getItemETH(
+          amount.mul(1000),
+          amount.mul(1000),
+          offererContract.address
+        ) as any,
+      ];
+
+      offer[0].identifier = offer[0].identifierOrCriteria;
+      offer[0].amount = offer[0].endAmount;
+
+      consideration[0].identifier = consideration[0].identifierOrCriteria;
+      consideration[0].amount = consideration[0].endAmount;
+
+      await offererContract
+        .connect(seller)
+        .activate(offer[0], consideration[0]);
+
+      consideration[0].startAmount = consideration[0].startAmount.sub(1);
+      consideration[0].endAmount = consideration[0].endAmount.sub(1);
+
+      const { order, value } = await createOrder(
+        seller,
+        zone,
+        offer,
+        consideration,
+        4 // CONTRACT
+      );
+
+      const { mirrorOrder, mirrorOrderHash } = await createMirrorBuyNowOrder(
+        buyer,
+        zone,
+        order
+      );
+
+      const contractOffererNonce =
+        await marketplaceContract.getContractOffererNonce(
+          offererContract.address
+        );
+
+      const orderHash =
+        offererContract.address.toLowerCase() +
+        contractOffererNonce.toHexString().slice(2).padStart(24, "0");
+
+      const orderStatus = await marketplaceContract.getOrderStatus(orderHash);
+
+      expect({ ...orderStatus }).to.deep.equal(
+        buildOrderStatus(false, false, 0, 0)
+      );
+
+      order.parameters.offerer = offererContract.address;
+      order.numerator = 1;
+      order.denominator = 1;
+      order.signature = "0x";
+
+      const orders = [order, mirrorOrder]
+
+      const offerComponents = [
+        [{ orderIndex: 0, itemIndex: 0 }],
+        [{ orderIndex: 1, itemIndex: 0 }],
+      ];
+      const considerationComponents = [
+        [{ orderIndex: 0, itemIndex: 0 }],
+        [{ orderIndex: 1, itemIndex: 0 }],
+      ];
+
+      await expect(
+        marketplaceContract
+          .connect(buyer)
+          .fulfillAvailableOrders(orders, offerComponents, considerationComponents, toKey(0), 100, {
+            value,
+          })
+      ).to.be.reverted;
+    });
     it("Can fulfill and aggregate contract orders via fulfillAvailableOrders with failing orders", async () => {
       // Seller mints nfts
       const { nftId: nftIdOne, amount: amountOne } = await mintAndApprove1155(
