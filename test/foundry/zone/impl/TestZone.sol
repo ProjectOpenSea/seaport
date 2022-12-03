897ed42debd93060e11475d8dea54a8b09b828b5
James Wenzel <jameswenzel@users.noreply.github.com>
2022-12-02 17:42:02 -0800

    remove old fn

--- test/foundry/zone/impl/TestZone.sol
+++ test/foundry/zone/impl/TestZone.sol
@@ -9,17 +9,6 @@ import {
 import { ZoneInterface } from "seaport/interfaces/ZoneInterface.sol";
 
 contract TestZone is ZoneInterface {
-    // Called by Consideration whenever extraData is not provided by the caller.
-    function isValidOrder(
-        bytes32 orderHash,
-        address caller,
-        address offerer,
-        bytes32 zoneHash
-    ) external view returns (bytes4 validOrderMagicValue) {
-        revert("hi");
-        return 0x0e1d31dc;
-    }
-
     // Called by Consideration whenever any extraData is provided by the caller.
     function validateOrder(ZoneParameters calldata zoneParameters)
         external
