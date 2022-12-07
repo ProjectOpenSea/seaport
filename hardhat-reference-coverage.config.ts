680f2b54f9bdc95ef64c0a3791a89ee949b8a7fd
0age <0age@protonmail.com>
2022-12-07 15:46:39 -0800

    use 0.8.13 for reference

--- hardhat-reference-coverage.config.ts
+++ hardhat-reference-coverage.config.ts
@@ -13,7 +13,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.8",
+        version: "0.8.13",
         settings: {
           viaIR: false,
           optimizer: {
