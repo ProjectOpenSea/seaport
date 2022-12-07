501757347e6900e5a4e86bcf149c66ae904cf163
0age <0age@protonmail.com>
2022-12-07 14:55:11 -0800

    bump reference to 0.8.8

--- hardhat-reference.config.ts
+++ hardhat-reference.config.ts
@@ -28,7 +28,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.7",
+        version: "0.8.8",
         settings: {
           viaIR: false,
           optimizer: {
