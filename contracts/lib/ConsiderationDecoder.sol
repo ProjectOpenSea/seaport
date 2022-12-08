e7468deb752a613b1fee651da939d1ec2bcc824d
0age <0age@protonmail.com>
2022-12-07 17:06:50 -0800

    remove unused decoders

--- contracts/lib/ConsiderationDecoder.sol
+++ contracts/lib/ConsiderationDecoder.sol
@@ -109,21 +109,6 @@ contract ConsiderationDecoder {
         }
     }
 
-    function abi_decode_BasicOrderParameters(
-        CalldataPointer cdPtr
-    ) internal pure returns (MemoryPointer mPtr) {
-        mPtr = malloc(BasicOrderParameters_head_size);
-        cdPtr.copy(mPtr, BasicOrderParameters_fixed_segment_0);
-        mPtr.offset(BasicOrderParameters_additionalRecipients_offset).write(
-            abi_decode_dyn_array_AdditionalRecipient(
-                cdPtr.pptr(BasicOrderParameters_additionalRecipients_offset)
-            )
-        );
-        mPtr.offset(BasicOrderParameters_signature_offset).write(
-            abi_decode_bytes(cdPtr.pptr(BasicOrderParameters_signature_offset))
-        );
-    }
-
     function abi_decode_dyn_array_OfferItem(
         CalldataPointer cdPtrLength
     ) internal pure returns (MemoryPointer mPtrLength) {
@@ -456,41 +441,6 @@ contract ConsiderationDecoder {
         }
     }
 
-    function abi_decode_OrderComponents(
-        CalldataPointer cdPtr
-    ) internal pure returns (MemoryPointer mPtr) {
-        mPtr = malloc(OrderComponents_head_size);
-        cdPtr.copy(mPtr, OrderComponents_head_size);
-        mPtr.offset(OrderComponents_offer_offset).write(
-            abi_decode_dyn_array_OfferItem(
-                cdPtr.pptr(OrderComponents_offer_offset)
-            )
-        );
-        mPtr.offset(OrderComponents_consideration_offset).write(
-            abi_decode_dyn_array_ConsiderationItem(
-                cdPtr.pptr(OrderComponents_consideration_offset)
-            )
-        );
-    }
-
-    function abi_decode_dyn_array_OrderComponents(
-        CalldataPointer cdPtrLength
-    ) internal pure returns (MemoryPointer mPtrLength) {
-        unchecked {
-            uint256 arrLength = cdPtrLength.readMaskedUint256();
-            uint256 tailOffset = arrLength * 32;
-            mPtrLength = malloc(tailOffset + 32);
-            mPtrLength.write(arrLength);
-            MemoryPointer mPtrHead = mPtrLength.next();
-            CalldataPointer cdPtrHead = cdPtrLength.next();
-            for (uint256 offset; offset < tailOffset; offset += 32) {
-                mPtrHead.offset(offset).write(
-                    abi_decode_OrderComponents(cdPtrHead.pptr(offset))
-                );
-            }
-        }
-    }
-
     function abi_decode_OrderComponents_as_OrderParameters(
         CalldataPointer cdPtr
     ) internal pure returns (MemoryPointer mPtr) {
@@ -509,37 +459,6 @@ contract ConsiderationDecoder {
         mPtr
             .offset(OrderParameters_totalOriginalConsiderationItems_offset)
             .write(consideration.read());
-    }
-
-    function to_BasicOrderParameters_ReturnType(
-        function(CalldataPointer) internal pure returns (MemoryPointer) inFn
-    )
-        internal
-        pure
-        returns (
-            function(CalldataPointer)
-                internal
-                pure
-                returns (BasicOrderParameters memory) outFn
-        )
-    {
-        assembly {
-            outFn := inFn
-        }
-    }
-
-    function to_Order_ReturnType(
-        function(CalldataPointer) internal pure returns (MemoryPointer) inFn
-    )
-        internal
-        pure
-        returns (
-            function(CalldataPointer) internal pure returns (Order memory) outFn
-        )
-    {
-        assembly {
-            outFn := inFn
-        }
     }
 
     function to_OrderParameters_ReturnType(
@@ -658,100 +577,6 @@ contract ConsiderationDecoder {
     {
         assembly {
             outFn := inFn
-        }
-    }
-
-    function to_dyn_array_OrderComponents_ReturnType(
-        function(CalldataPointer) internal pure returns (MemoryPointer) inFn
-    )
-        internal
-        pure
-        returns (
-            function(CalldataPointer)
-                internal
-                pure
-                returns (OrderComponents[] memory) outFn
-        )
-    {
-        assembly {
-            outFn := inFn
-        }
-    }
-
-    function to_OrderComponents_ReturnType(
-        function(CalldataPointer) internal pure returns (MemoryPointer) inFn
-    )
-        internal
-        pure
-        returns (
-            function(CalldataPointer)
-                internal
-                pure
-                returns (OrderComponents memory) outFn
-        )
-    {
-        assembly {
-            outFn := inFn
-        }
-    }
-
-    function return_bool(bool fulfilled) internal pure {
-        bytes memory returnData = abi.encode(fulfilled);
-        assembly {
-            return(add(returnData, 32), mload(returnData))
-        }
-    }
-
-    function return_uint256(uint256 newCounter) internal pure {
-        bytes memory returnData = abi.encode(newCounter);
-        assembly {
-            return(add(returnData, 32), mload(returnData))
-        }
-    }
-
-    function return_bytes32(bytes32 orderHash) internal pure {
-        bytes memory returnData = abi.encode(orderHash);
-        assembly {
-            return(add(returnData, 32), mload(returnData))
-        }
-    }
-
-    function return_tuple_bool_bool_uint256_uint256(
-        bool isValidated,
-        bool isCancelled,
-        uint256 totalFilled,
-        uint256 totalSize
-    ) internal pure {
-        bytes memory returnData = abi.encode(
-            isValidated,
-            isCancelled,
-            totalFilled,
-            totalSize
-        );
-        assembly {
-            return(add(returnData, 32), mload(returnData))
-        }
-    }
-
-    function return_tuple_string_bytes32_address(
-        string memory version,
-        bytes32 domainSeparator,
-        address conduitController
-    ) internal pure {
-        bytes memory returnData = abi.encode(
-            version,
-            domainSeparator,
-            conduitController
-        );
-        assembly {
-            return(add(returnData, 32), mload(returnData))
-        }
-    }
-
-    function return_string(string memory value0) internal pure {
-        bytes memory returnData = abi.encode(value0);
-        assembly {
-            return(add(returnData, 32), mload(returnData))
         }
     }
 }
