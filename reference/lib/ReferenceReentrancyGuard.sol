e4c6237651aed43a6a5f1a888ab0f98153e907e4
Stephan Min <stephan.min@opensea.io>
2022-12-09 13:26:33 -0500

    rm nonReentrant

--- reference/lib/ReferenceReentrancyGuard.sol
+++ reference/lib/ReferenceReentrancyGuard.sol
@@ -24,16 +24,6 @@ contract ReferenceReentrancyGuard is ReentrancyErrors {
     }
 
     /**
-     * @dev Modifier to set the reentrancy guard sentinel value for the duration
-     *      of the call.
-     */
-    modifier nonReentrant() {
-        _reentrancyGuard = _ENTERED;
-        _;
-        _reentrancyGuard = _NOT_ENTERED;
-    }
-
-    /**
      * @dev Modifier to check that the sentinel value for the reentrancy guard
      *      is not currently set by a previous call.
      */
