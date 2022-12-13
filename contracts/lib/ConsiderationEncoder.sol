467fcb3280c764dc07544dcd9fe2ce3626fc66cb
d1ll0n <dillon@indexed.finance>
2022-12-13 13:26:52 -0600

    Use solidity for abi_encode_dyn_array_ConsiderationItem_as_dyn_array_ReceivedItem

--- contracts/lib/ConsiderationEncoder.sol
+++ contracts/lib/ConsiderationEncoder.sol
@@ -452,52 +452,27 @@ contract ConsiderationEncoder {
     function abi_encode_dyn_array_ConsiderationItem_as_dyn_array_ReceivedItem(
         MemoryPointer srcLength,
         MemoryPointer dstLength
-    ) internal pure returns (uint256 size) {
-        assembly {
-            let length := mload(srcLength)
-            mstore(dstLength, length)
+    ) internal view returns (uint256 size) {
+        unchecked {
+            uint256 length = srcLength.readUint256();
+            dstLength.write(length);
 
             // Get pointer to first item's head position in the array, containing
             // the item's pointer in memory. The head pointer will be incremented
             // until it reaches the tail position (start of the array data).
-            let mPtrHead := add(srcLength, 0x20)
+            MemoryPointer srcHead = srcLength.next();
+            MemoryPointer srcHeadEnd = srcHead.offset(length * OneWord);
             // Position in memory to write next item for calldata. Since ReceivedItem
-            // has a fixed length, the array elements do not contain head elements in
+            // has a fixed length, the array elements do not contain offsets in
             // calldata, they are concatenated together after the array length.
-            let cdPtrData := add(dstLength, 0x20)
-            // Pointer to end of array head in memory.
-            let mPtrHeadEnd := add(mPtrHead, mul(length, 0x20))
-
-            for {
-
-            } lt(mPtrHead, mPtrHeadEnd) {
-
-            } {
-                // Read pointer to data for the array element from its head position
-                let mPtrTail := mload(mPtrHead)
-                // Copy the itemType, token, identifier, amount from the item to calldata
-                mstore(cdPtrData, mload(mPtrTail))
-                mstore(
-                    add(cdPtrData, Common_token_offset),
-                    mload(add(mPtrTail, Common_token_offset))
-                )
-                mstore(
-                    add(cdPtrData, Common_identifier_offset),
-                    mload(add(mPtrTail, Common_identifier_offset))
-                )
-                mstore(
-                    add(cdPtrData, Common_amount_offset),
-                    mload(add(mPtrTail, Common_amount_offset))
-                )
-                mstore(
-                    add(cdPtrData, ReceivedItem_recipient_offset),
-                    mload(add(mPtrTail, Common_endAmount_offset))
-                )
-
-                mPtrHead := add(mPtrHead, 0x20)
-                cdPtrData := add(cdPtrData, ReceivedItem_size)
+            MemoryPointer dstHead = dstLength.next();
+            while (srcHead.lt(srcHeadEnd)) {
+                MemoryPointer srcTail = srcHead.pptr();
+                srcTail.copy(dstHead, ReceivedItem_size);
+                srcHead = srcHead.next();
+                dstHead = dstHead.offset(ReceivedItem_size);
             }
-            size := add(0x20, mul(length, ReceivedItem_size))
+            size = 32 + (length * ReceivedItem_size);
         }
     }
 }
