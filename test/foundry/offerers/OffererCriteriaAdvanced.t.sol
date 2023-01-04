f197c975b1c21329f7d4790642ba4e6014c3fed9
James Wenzel <wenzel.james.r@gmail.com>
2023-01-03 22:03:34 -0500

    add tests for recipients and fractions

--- test/foundry/offerers/OffererCriteriaAdvanced.t.sol
+++ test/foundry/offerers/OffererCriteriaAdvanced.t.sol
@@ -26,7 +26,18 @@ import {
     CriteriaResolver
 } from "../../../contracts/lib/ConsiderationStructs.sol";
 
-contract OffererCriteriaAdvancedTest is BaseOrderTest {
+import {
+    ConsiderationEventsAndErrors
+} from "../../../contracts/interfaces/ConsiderationEventsAndErrors.sol";
+import {
+    ZoneInteractionErrors
+} from "../../../contracts/interfaces/ZoneInteractionErrors.sol";
+
+contract OffererCriteriaAdvancedTest is
+    BaseOrderTest,
+    ConsiderationEventsAndErrors,
+    ZoneInteractionErrors
+{
     PassthroughOfferer offerer;
     CriteriaResolver[] criteriaResolvers;
 
@@ -57,6 +68,154 @@ contract OffererCriteriaAdvancedTest is BaseOrderTest {
         try fn(context) {} catch (bytes memory reason) {
             assertPass(reason);
         }
+    }
+
+    function testOnlyWholeFractional1() public {
+        setUpOnlyWholeFractional();
+
+        test(
+            this.execOnlyWholeFractional1,
+            Context({ seaport: consideration })
+        );
+        test(
+            this.execOnlyWholeFractional1,
+            Context({ seaport: referenceConsideration })
+        );
+    }
+
+    function setUpOnlyWholeFractional() public {
+        setUpNormalOrder(address(offerer));
+    }
+
+    function setUpNormalOrder(address recipient) public {
+        // add normal offer item identifier 2
+        addOfferItem({
+            itemType: ItemType.ERC721,
+            token: address(test721_1),
+            identifier: 2,
+            amount: 1
+        });
+        // add consideration item to address(test) for 1000 of token1
+        addConsiderationItem(
+            ConsiderationItem({
+                itemType: ItemType.ERC20,
+                token: address(token1),
+                identifierOrCriteria: 0,
+                startAmount: 1000,
+                endAmount: 1000,
+                recipient: payable(recipient)
+            })
+        );
+    }
+
+    function execOnlyWholeFractional1(
+        Context memory context
+    ) external stateless {
+        fulfillAdvanced(context, configureAdvancedOrder());
+        assertEq(test721_1.ownerOf(2), address(this));
+    }
+
+    // same as above but test a 0/n fraction
+    function testOnlyWholeFractional0() public {
+        setUpOnlyWholeFractional();
+
+        test(
+            this.execOnlyWholeFractional0,
+            Context({ seaport: consideration })
+        );
+        test(
+            this.execOnlyWholeFractional0,
+            Context({ seaport: referenceConsideration })
+        );
+    }
+
+    function execOnlyWholeFractional0(
+        Context memory context
+    ) external stateless {
+        vm.expectRevert(BadFraction.selector);
+        fulfillAdvanced(context, configureAdvancedOrder(0, 1));
+    }
+
+    // same as above but test a n/n fraction (2/2)
+    function testOnlyWholeFractional2() public {
+        setUpOnlyWholeFractional();
+
+        test(
+            this.execOnlyWholeFractional2,
+            Context({ seaport: consideration })
+        );
+        test(
+            this.execOnlyWholeFractional2,
+            Context({ seaport: referenceConsideration })
+        );
+    }
+
+    function execOnlyWholeFractional2(
+        Context memory context
+    ) external stateless {
+        vm.expectRevert(BadFraction.selector);
+        fulfillAdvanced(context, configureAdvancedOrder(2, 2));
+    }
+
+    function setUpOnlyWholeFractional0() public {
+        // add normal offer item identifier 2
+        addOfferItem({
+            itemType: ItemType.ERC721,
+            token: address(test721_1),
+            identifier: 2,
+            amount: 1
+        });
+        // add consideration item to address(test) for 1000 of token1
+        addConsiderationItem(
+            ConsiderationItem({
+                itemType: ItemType.ERC20,
+                token: address(token1),
+                identifierOrCriteria: 0,
+                startAmount: 0,
+                endAmount: 0,
+                recipient: payable(address(offerer))
+            })
+        );
+    }
+
+    function testZeroReceiverWildcard() public {
+        setUpNormalOrder(address(0));
+
+        test(
+            this.execZeroReceiverWildcard,
+            Context({ seaport: consideration })
+        );
+        test(
+            this.execZeroReceiverWildcard,
+            Context({ seaport: referenceConsideration })
+        );
+    }
+
+    function execZeroReceiverWildcard(
+        Context memory context
+    ) external stateless {
+        fulfillAdvanced(context, configureAdvancedOrder(1, 1));
+        assertEq(test721_1.ownerOf(2), address(this));
+    }
+
+    function testMismatchedReceiver() public {
+        setUpNormalOrder(makeAddr("not offerer"));
+
+        test(this.execMismatchedReceiver, Context({ seaport: consideration }));
+        test(
+            this.execMismatchedReceiver,
+            Context({ seaport: referenceConsideration })
+        );
+    }
+
+    function execMismatchedReceiver(Context memory context) external stateless {
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                InvalidContractOrder.selector,
+                (uint256(uint160(address(offerer)))) << 96
+            )
+        );
+        fulfillAdvanced(context, configureAdvancedOrder(1, 1));
     }
 
     function testCriteriaMinimumReceived() public {
@@ -102,7 +261,6 @@ contract OffererCriteriaAdvancedTest is BaseOrderTest {
                 criteriaProof: proof
             })
         );
-        configureAdvancedOrder();
     }
 
     function execCriteriaMinimumReceived(
@@ -159,25 +317,31 @@ contract OffererCriteriaAdvancedTest is BaseOrderTest {
                 criteriaProof: proof
             })
         );
-        configureAdvancedOrder();
     }
 
     function execCriteriaMaximumSpent(
         Context memory context
     ) external stateless {
         fulfillAdvanced(context, configureAdvancedOrder());
-        // assertEq(test721_1.ownerOf(1), address(offerer));
+        assertEq(test721_1.ownerOf(1), address(offerer));
     }
 
     function configureAdvancedOrder() internal returns (AdvancedOrder memory) {
+        return configureAdvancedOrder(1, 1);
+    }
+
+    function configureAdvancedOrder(
+        uint120 numer,
+        uint120 denom
+    ) internal returns (AdvancedOrder memory) {
         return
             AdvancedOrder({
                 parameters: getOrderParameters(
                     address(offerer),
                     OrderType.CONTRACT
                 ),
-                numerator: 1,
-                denominator: 1,
+                numerator: numer,
+                denominator: denom,
                 signature: "",
                 extraData: ""
             });
