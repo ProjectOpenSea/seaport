b4d49ccdf6bb6c09162955eb71836528d44f9d2f
0age <0age@protonmail.com>
2023-01-09 15:24:38 -0800

    name it correctly

--- test/revert.spec.ts
+++ test/revert.spec.ts
@@ -5481,7 +5481,7 @@ describe(`Reverts (Seaport v${VERSION})`, function () {
         getItemETH(
           parseEther("1"),
           parseEther("1"),
-          TestERC20.address
+          testERC20.address
         ),
       ];
 
@@ -6862,7 +6862,7 @@ describe(`Reverts (Seaport v${VERSION})`, function () {
       // Seller mints nft
       const nftId = await mintAndApprove721(
         seller,
-        TestERC20.address
+        testERC20.address
       );
 
       const offer = [getTestItem721(nftId)];
