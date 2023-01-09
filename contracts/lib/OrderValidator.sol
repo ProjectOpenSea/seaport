e180c5386eea103a456487d637ce201529d3be16
djviau <dan@opensea.io>
2023-01-09 12:08:08 -0500

    document contract order collection offer behavior

--- contracts/lib/OrderValidator.sol
+++ contracts/lib/OrderValidator.sol
@@ -366,7 +366,12 @@ contract OrderValidator is Executor, ZoneInteraction {
 
     /**
      * @dev Internal pure function to check the compatibility of two offer
-     *      or consideration items for contract orders.
+     *      or consideration items for contract orders.  Note that the itemType
+     *      and identifier are reset in cases where criteria = 0 (collection-
+     *      wide offers), which means that a contract offerer has full latitude
+     *      to choose any identifier it wants mid-flight, in contrast to the
+     *      normal behavior, where the fulfiller can pick which identifier to
+     *      receive by providing a CriteriaResolver.
      *
      * @param originalItem The original offer or consideration item.
      * @param newItem      The new offer or consideration item.
@@ -444,7 +449,15 @@ contract OrderValidator is Executor, ZoneInteraction {
     }
 
     /**
-     * @dev Internal function to generate a contract order.
+     * @dev Internal function to generate a contract order. If you supply a
+     *      collection-wide criteria-based item (criteria = 0) as an input to a
+     *      contract order, the contract offerer has full latitude to choose any
+     *      identifier it wants mid-flight, which differs from the usual
+     *      behavior.  For regular criteria-based orders with
+     *      identifierOrCriteria = 0, the fulfiller can pick which identifier to
+     *      receive by providing a CriteriaResolver. For contract offers with
+     *      identifierOrCriteria = 0, Seaport does not expect a corresponding
+     *      CriteriaResolver, and will revert if one is provided.
      *
      * @param orderParameters The parameters for the order.
      * @param context         The context for generating the order.
