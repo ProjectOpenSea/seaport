6115e27aa4053eafef2eb9fed3836405d62968fa
0age <0age@protonmail.com>
2023-01-12 11:55:25 -0800

    include script in linter and clean up

--- docs/prepare-docs.js
+++ docs/prepare-docs.js
@@ -3,26 +3,28 @@ const glob = require("glob");
 
 /**
  * This script is used to prepare the solidity files for use with `forge doc`
- * Seaport handles decoding structs from calldata itself, but solc will use its default decoding
- * if they are given names. Thus, to comply with natspec, we use  @custom tags to specify "unnamed" params
- * and their names. This is not compatible with forge doc, so we must turn them back into normal @param tags
- * before generating documentation.
+ * Seaport handles decoding structs from calldata itself, but solc will use its
+ * default decoding if given names. Thus, to comply with natspec, use @custom
+ * tags to specify "unnamed" params and their names. This is not compatible with
+ * forge doc, so they are turned back into normal @param tags before generating
+ * documentation.
  */
 
 glob("contracts/**/*.sol", {}, (er, files) => {
   files.forEach((file) => {
     let content = fs.readFileSync(file, "utf-8");
 
-    // Restore normal @param tags
+    // Restore normal @param tags.
     content = content.replace(/@custom:param/g, "@param");
-    // Replace @custom:name tags with the name of the param in the correct location
+
+    // Replace @custom:name tags with name of the param in the correct location.
     content = content.replace(
       /(,|\))\s*\/\*\*\s*\*?\s*@custom:name\s*( [^*]*)\s*\*\/\s*/g,
       "$2$1"
     );
 
-    // once we overwrite the files, we can call forge doc to generate documentation complete with descriptions for
-    // "unnamed" variables
+    // Once files have been overwritten, call forge doc to generate
+    // documentation complete with descriptions for "unnamed" variables.
     fs.writeFileSync(file, content, "utf-8");
   });
 });
