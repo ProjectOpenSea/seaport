08a89e4ea944eb22376c639bb4b2078fe09f7263
djviau <dan@opensea.io>
2023-01-26 09:06:29 -0500

    name the params in the token interfaces

--- contracts/interfaces/AbridgedTokenInterfaces.sol
+++ contracts/interfaces/AbridgedTokenInterfaces.sol
@@ -9,21 +9,31 @@ interface ERC20Interface {
     /**
      * @dev Allows an operator to transfer tokens on behalf of an owner.
      *
-     * @ param address The address of the owner.
-     * @ param address The address of the recipient.
-     * @ param uint256 The amount of tokens to transfer.
+     * @param from  The address of the owner.
+     * @param to    The address of the recipient.
+     * @param value The amount of tokens to transfer.
+     *
+     * @return success True if the transfer was successful.
      */
-    function transferFrom(address, address, uint256) external returns (bool);
+    function transferFrom(
+        address from,
+        address to,
+        uint256 value
+    ) external returns (bool success);
 
     /**
      * @dev Allows an operator to approve a spender to transfer tokens on behalf
      *      of a user.
      *
-     * @ param address The address of the user.
-     * @ param address The address of the spender.
-     * @ param uint256 The amount of tokens to approve.
+     * @param spender The address of the spender.
+     * @param value   The amount of tokens to approve.
+     *
+     * @return success True if the approval was successful.
      */
-    function approve(address, uint256) external returns (bool);
+    function approve(
+        address spender,
+        uint256 value
+    ) external returns (bool success);
 }
 
 /**
@@ -34,29 +44,29 @@ interface ERC721Interface {
     /**
      * @dev Allows an operator to transfer tokens on behalf of an owner.
      *
-     * @ param address The address of the owner.
-     * @ param address The address of the recipient.
-     * @ param uint256 The ID of the token to transfer.
+     * @param from    The address of the owner.
+     * @param to      The address of the recipient.
+     * @param tokenId The ID of the token to transfer.
      */
-    function transferFrom(address, address, uint256) external;
+    function transferFrom(address from, address to, uint256 tokenId) external;
 
     /**
      * @dev Allows an owner to approve an operator to transfer all tokens on a
      *      contract on behalf of the owner.
      *
-     * @ param address The address of the operator.
-     * @ param bool    Whether the operator is approved.
+     * @param to       The address of the operator.
+     * @param approved Whether the operator is approved.
      */
-    function setApprovalForAll(address, bool) external;
+    function setApprovalForAll(address to, bool approved) external;
 
     /**
      * @dev Returns the owner of a given token ID.
      *
-     * @ param uint256 The token ID.
+     * @param tokenId The token ID.
      *
-     * @ return address The owner of the token.
+     * @return owner The owner of the token.
      */
-    function ownerOf(uint256) external view returns (address);
+    function ownerOf(uint256 tokenId) external view returns (address owner);
 }
 
 /**
@@ -102,8 +112,8 @@ interface ERC1155Interface {
      * @dev Allows an owner to approve an operator to transfer all tokens on a
      *      contract on behalf of the owner.
      *
-     * @ param address The address of the operator.
-     * @ param bool    Whether the operator is approved.
+     * @param to       The address of the operator.
+     * @param approved Whether the operator is approved.
      */
-    function setApprovalForAll(address, bool) external;
+    function setApprovalForAll(address to, bool approved) external;
 }
