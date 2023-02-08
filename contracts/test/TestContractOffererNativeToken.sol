e7a271653db1cac0a7e756708673082262ac0d04
Stephan Min <stephankmin@gmail.com>
2023-02-08 16:19:42 -0500

    bump

--- contracts/test/TestContractOffererNativeToken.sol
+++ contracts/test/TestContractOffererNativeToken.sol
@@ -10,9 +10,7 @@ import {
     ContractOffererInterface
 } from "../interfaces/ContractOffererInterface.sol";
 
-import {
-    ERC165
-} from "openzeppelin-contracts/contracts/utils/introspection/ERC165.sol";
+import { ERC165 } from "../interfaces/ERC165.sol";
 
 import { ItemType } from "../lib/ConsiderationEnums.sol";
 
