73967fabce31e2f0a3bbdf6839be6a556df5e580
Stephan Min <stephankmin@gmail.com>
2023-02-09 13:11:34 -0500

    separate interfaces

--- contracts/order-validator/SeaportValidator.sol
+++ contracts/order-validator/SeaportValidator.sol
@@ -32,6 +32,7 @@ import {
     ERC1155Interface
 } from "../interfaces/AbridgedTokenInterfaces.sol";
 import { IERC165 } from "../interfaces/IERC165.sol";
+import { IERC2981 } from "../interfaces/IERC2981.sol";
 import {
     ErrorsAndWarnings,
     ErrorsAndWarningsLib
@@ -1655,23 +1656,4 @@ interface CreatorFeeEngineInterface {
         external
         view
         returns (address payable[] memory recipients, uint256[] memory amounts);
-}
-
-/**
- * @dev Interface for the NFT Royalty Standard.
- *
- * A standardized way to retrieve royalty payment information for non-fungible tokens (NFTs) to enable universal
- * support for royalty payments across all NFT marketplaces and ecosystem participants.
- *
- * _Available since v4.5._
- */
-interface IERC2981 is IERC165 {
-    /**
-     * @dev Returns how much royalty is owed and to whom, based on a sale price that may be denominated in any unit of
-     * exchange. The royalty amount is denominated and should be paid in that same unit of exchange.
-     */
-    function royaltyInfo(
-        uint256 tokenId,
-        uint256 salePrice
-    ) external view returns (address receiver, uint256 royaltyAmount);
 }
