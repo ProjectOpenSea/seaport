0587119df59ae660bc4157323bfa6a6493c2ef8f
Stephan Min <stephankmin@gmail.com>
2023-03-07 16:04:46 -0500

    add validator config

---
+++ hardhat-validator.config.ts
@@ -0,0 +1,165 @@
+import { TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS } from "hardhat/builtin-tasks/task-names";
+import { subtask, task } from "hardhat/config";
+
+import { compareLastTwoReports } from "./scripts/compare_reports";
+import { printLastReport } from "./scripts/print_report";
+import { getReportPathForCommit } from "./scripts/utils";
+import { writeReports } from "./scripts/write_reports";
+
+import type { HardhatUserConfig } from "hardhat/config";
+
+import "dotenv/config";
+import "@nomiclabs/hardhat-ethers";
+import "@nomicfoundation/hardhat-chai-matchers";
+import "@nomiclabs/hardhat-etherscan";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+
+// Filter Reference Contracts
+subtask(TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS).setAction(
+  async (_, __, runSuper) => {
+    const paths = await runSuper();
+
+    return paths.filter((p: any) => !p.includes("contracts/reference/"));
+  }
+);
+
+task("write-reports", "Write pending gas reports").setAction(
+  async (taskArgs, hre) => {
+    writeReports(hre);
+  }
+);
+
+task("compare-reports", "Compare last two gas reports").setAction(
+  async (taskArgs, hre) => {
+    compareLastTwoReports(hre);
+  }
+);
+
+task("print-report", "Print the last gas report").setAction(
+  async (taskArgs, hre) => {
+    printLastReport(hre);
+  }
+);
+
+const optimizerSettingsNoSpecializer = {
+  enabled: true,
+  runs: 4_294_967_295,
+  details: {
+    peephole: true,
+    inliner: true,
+    jumpdestRemover: true,
+    orderLiterals: true,
+    deduplicate: true,
+    cse: true,
+    constantOptimizer: true,
+    yulDetails: {
+      stackAllocation: true,
+      optimizerSteps:
+        "dhfoDgvulfnTUtnIf[xa[r]EscLMcCTUtTOntnfDIulLculVcul [j]Tpeulxa[rul]xa[r]cLgvifCTUca[r]LSsTOtfDnca[r]Iulc]jmul[jul] VcTOcul jmul",
+    },
+  },
+};
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.17",
+        settings: {
+          viaIR: true,
+          optimizer: {
+            ...(process.env.NO_SPECIALIZER
+              ? optimizerSettingsNoSpecializer
+              : { enabled: true, runs: 4_294_967_295 }),
+          },
+          metadata: {
+            bytecodeHash: "none",
+          },
+          outputSelection: {
+            "*": {
+              "*": ["evm.assembly", "irOptimized", "devdoc"],
+            },
+          },
+        },
+      },
+    ],
+    overrides: {
+      "contracts/conduit/Conduit.sol": {
+        version: "0.8.14",
+        settings: {
+          viaIR: true,
+          optimizer: {
+            enabled: true,
+            runs: 1000000,
+          },
+        },
+      },
+      "contracts/conduit/ConduitController.sol": {
+        version: "0.8.14",
+        settings: {
+          viaIR: true,
+          optimizer: {
+            enabled: true,
+            runs: 1000000,
+          },
+        },
+      },
+      "contracts/helpers/TransferHelper.sol": {
+        version: "0.8.14",
+        settings: {
+          viaIR: true,
+          optimizer: {
+            enabled: true,
+            runs: 1000000,
+          },
+        },
+      },
+      "order-validator/SeaportValidator.sol": {
+        version: "0.8.17",
+        settings: {
+          viaIR: false,
+          optimizer: {
+            enabled: true,
+            runs: 1,
+          },
+        },
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      blockGasLimit: 30_000_000,
+      throwOnCallFailures: false,
+      allowUnlimitedContractSize: true,
+      forking: {
+        enabled: true,
+        url: process.env.ETH_RPC_URL ?? "",
+      },
+    },
+    verificationNetwork: {
+      url: process.env.NETWORK_RPC ?? "",
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+    outputFile: getReportPathForCommit(),
+    noColors: true,
+  },
+  etherscan: {
+    apiKey: process.env.EXPLORER_API_KEY,
+  },
+  // specify separate cache for hardhat, since it could possibly conflict with foundry's
+  paths: {
+    sources: "./order-validator",
+    tests: "./order-validator/test",
+    cache: "hh-cache",
+  },
+};
+
+export default config;
---
+++ order-validator/test/ValidateOrderArbitrum.spec.ts
@@ -0,0 +1,272 @@
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+import {
+  CROSS_CHAIN_SEAPORT_ADDRESS,
+  ConsiderationIssue,
+  EIP_712_ORDER_TYPE,
+  EMPTY_BYTES32,
+  ItemType,
+  NULL_ADDRESS,
+  OrderType,
+} from "./order-validator-constants";
+
+import type {
+  ConsiderationInterface,
+  SeaportValidator,
+  TestERC1155,
+  TestERC721Fee,
+  TestERC721Funky,
+} from "../typechain-types";
+import type { OrderComponentsStruct } from "../typechain-types/contracts/interfaces/ConsiderationInterface";
+import type {
+  OrderParametersStruct,
+  OrderStruct,
+} from "../typechain-types/contracts/order-validator/SeaportValidator.sol/SeaportValidator";
+import type { TestERC20 } from "../typechain-types/contracts/test/TestERC20";
+import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+
+describe("Validate Orders (Arbitrum)", function () {
+  const feeRecipient = "0x0000000000000000000000000000000000000FEE";
+  const coder = new ethers.utils.AbiCoder();
+  let baseOrderParameters: OrderParametersStruct;
+  let validator: SeaportValidator;
+  let seaport: ConsiderationInterface;
+  let owner: SignerWithAddress;
+  let otherAccounts: SignerWithAddress[];
+  let erc721_1: TestERC721Fee;
+  let erc721_2: TestERC721Fee;
+  let erc1155_1: TestERC1155;
+  let erc20_1: TestERC20;
+  let erc721_funky: TestERC721Funky;
+
+  before(async function () {
+    seaport = await ethers.getContractAt(
+      "ConsiderationInterface",
+      CROSS_CHAIN_SEAPORT_ADDRESS
+    );
+  });
+
+  async function deployFixture() {
+    const [owner, ...otherAccounts] = await ethers.getSigners();
+
+    const Validator = await ethers.getContractFactory("SeaportValidator");
+    const TestERC721Factory = await ethers.getContractFactory("TestERC721Fee");
+    const TestERC1155Factory = await ethers.getContractFactory("TestERC1155");
+    const TestERC20Factory = await ethers.getContractFactory("TestERC20");
+    const TestERC721FunkyFactory = await ethers.getContractFactory(
+      "TestERC721Funky"
+    );
+
+    const validator = await Validator.deploy();
+
+    const erc721_1 = await TestERC721Factory.deploy();
+    const erc721_2 = await TestERC721Factory.deploy();
+    const erc1155_1 = await TestERC1155Factory.deploy();
+    const erc20_1 = await TestERC20Factory.deploy();
+    const erc721_funky = await TestERC721FunkyFactory.deploy();
+
+    return {
+      validator,
+      owner,
+      otherAccounts,
+      erc721_1,
+      erc721_2,
+      erc1155_1,
+      erc20_1,
+      erc721_funky,
+    };
+  }
+
+  beforeEach(async function () {
+    const res = await loadFixture(deployFixture);
+    validator = res.validator;
+    owner = res.owner;
+    otherAccounts = res.otherAccounts;
+    erc721_1 = res.erc721_1;
+    erc721_2 = res.erc721_2;
+    erc1155_1 = res.erc1155_1;
+    erc20_1 = res.erc20_1;
+    erc721_funky = res.erc721_funky;
+
+    baseOrderParameters = {
+      offerer: owner.address,
+      zone: NULL_ADDRESS,
+      orderType: OrderType.FULL_OPEN,
+      startTime: "0",
+      endTime: Math.round(Date.now() / 1000 + 4000).toString(),
+      salt: "0",
+      totalOriginalConsiderationItems: 0,
+      offer: [],
+      consideration: [],
+      zoneHash: EMPTY_BYTES32,
+      conduitKey: EMPTY_BYTES32,
+    };
+  });
+
+  describe("Check Creator Fees", function () {
+    // We are checking creator fees solely based on EIP2981 here
+
+    it("Check creator fees success", async function () {
+      // Enable creator fees on token
+      await erc721_1.setCreatorFeeEnabled(true);
+
+      baseOrderParameters.offer = [
+        {
+          itemType: ItemType.ERC20,
+          token: erc20_1.address,
+          identifierOrCriteria: "0",
+          startAmount: "1000",
+          endAmount: "1000",
+        },
+      ];
+      baseOrderParameters.consideration = [
+        {
+          itemType: ItemType.ERC721,
+          token: erc721_1.address,
+          identifierOrCriteria: "1",
+          startAmount: "1",
+          endAmount: "1",
+          recipient: owner.address,
+        },
+        {
+          itemType: ItemType.ERC20,
+          token: erc20_1.address,
+          identifierOrCriteria: "0",
+          startAmount: "25",
+          endAmount: "25",
+          recipient: feeRecipient,
+        },
+        {
+          itemType: ItemType.ERC20,
+          token: erc20_1.address,
+          identifierOrCriteria: "0",
+          startAmount: "25",
+          endAmount: "25",
+          recipient: "0x000000000000000000000000000000000000FEE2",
+        },
+      ];
+
+      expect(
+        await validator.validateStrictLogic(
+          baseOrderParameters,
+          feeRecipient,
+          "250",
+          true
+        )
+      ).to.include.deep.ordered.members([[], []]);
+    });
+
+    it("Check creator fees reverts", async function () {
+      baseOrderParameters.offer = [
+        {
+          itemType: ItemType.ERC20,
+          token: erc20_1.address,
+          identifierOrCriteria: "0",
+          startAmount: "1000",
+          endAmount: "1000",
+        },
+      ];
+      baseOrderParameters.consideration = [
+        {
+          itemType: ItemType.ERC721,
+          token: erc721_1.address,
+          identifierOrCriteria: "1",
+          startAmount: "1",
+          endAmount: "1",
+          recipient: owner.address,
+        },
+        {
+          itemType: ItemType.ERC20,
+          token: erc20_1.address,
+          identifierOrCriteria: "0",
+          startAmount: "25",
+          endAmount: "25",
+          recipient: "0x000000000000000000000000000000000000FEE2",
+        },
+      ];
+
+      expect(
+        await validator.validateStrictLogic(
+          baseOrderParameters,
+          NULL_ADDRESS,
+          "0",
+          true
+        )
+      ).to.include.deep.ordered.members([[ConsiderationIssue.ExtraItems], []]);
+    });
+
+    it("Check creator fees returns unexpected value", async function () {
+      baseOrderParameters.offer = [
+        {
+          itemType: ItemType.ERC20,
+          token: erc20_1.address,
+          identifierOrCriteria: "0",
+          startAmount: "1000",
+          endAmount: "1000",
+        },
+      ];
+      baseOrderParameters.consideration = [
+        {
+          itemType: ItemType.ERC721,
+          token: erc721_funky.address,
+          identifierOrCriteria: "1",
+          startAmount: "1",
+          endAmount: "1",
+          recipient: owner.address,
+        },
+        {
+          itemType: ItemType.ERC20,
+          token: erc20_1.address,
+          identifierOrCriteria: "0",
+          startAmount: "25",
+          endAmount: "25",
+          recipient: "0x000000000000000000000000000000000000FEE2",
+        },
+      ];
+
+      expect(
+        await validator.validateStrictLogic(
+          baseOrderParameters,
+          NULL_ADDRESS,
+          "0",
+          true
+        )
+      ).to.include.deep.ordered.members([[ConsiderationIssue.ExtraItems], []]);
+    });
+  });
+
+  async function signOrder(
+    orderParameters: OrderParametersStruct,
+    signer: SignerWithAddress,
+    counter?: number
+  ): Promise<OrderStruct> {
+    const sig = await signer._signTypedData(
+      {
+        name: "Seaport",
+        version: "1.1",
+        chainId: "1",
+        verifyingContract: seaport.address,
+      },
+      EIP_712_ORDER_TYPE,
+      await getOrderComponents(orderParameters, signer, counter)
+    );
+
+    return {
+      parameters: orderParameters,
+      signature: sig,
+    };
+  }
+
+  async function getOrderComponents(
+    orderParameters: OrderParametersStruct,
+    signer: SignerWithAddress,
+    counter?: number
+  ): Promise<OrderComponentsStruct> {
+    return {
+      ...orderParameters,
+      counter: counter ?? (await seaport.getCounter(signer.address)),
+    };
+  }
+});
