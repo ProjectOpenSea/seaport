50a0ed2e2dd669eeaebeff5513d6fbf1e83921c5
Stephan Min <stephankmin@gmail.com>
2023-03-07 16:10:11 -0500

    update hh config

--- hardhat-validator.config.ts
+++ hardhat-validator.config.ts
@@ -21,46 +21,9 @@ subtask(TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS).setAction(
   async (_, __, runSuper) => {
     const paths = await runSuper();
 
-    return paths.filter((p: any) => !p.includes("contracts/reference/"));
+    return paths.filter((p: any) => !p.includes("contracts/"));
   }
 );
-
-task("write-reports", "Write pending gas reports").setAction(
-  async (taskArgs, hre) => {
-    writeReports(hre);
-  }
-);
-
-task("compare-reports", "Compare last two gas reports").setAction(
-  async (taskArgs, hre) => {
-    compareLastTwoReports(hre);
-  }
-);
-
-task("print-report", "Print the last gas report").setAction(
-  async (taskArgs, hre) => {
-    printLastReport(hre);
-  }
-);
-
-const optimizerSettingsNoSpecializer = {
-  enabled: true,
-  runs: 4_294_967_295,
-  details: {
-    peephole: true,
-    inliner: true,
-    jumpdestRemover: true,
-    orderLiterals: true,
-    deduplicate: true,
-    cse: true,
-    constantOptimizer: true,
-    yulDetails: {
-      stackAllocation: true,
-      optimizerSteps:
-        "dhfoDgvulfnTUtnIf[xa[r]EscLMcCTUtTOntnfDIulLculVcul [j]Tpeulxa[rul]xa[r]cLgvifCTUca[r]LSsTOtfDnca[r]Iulc]jmul[jul] VcTOcul jmul",
-    },
-  },
-};
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -71,57 +34,6 @@ const config: HardhatUserConfig = {
       {
         version: "0.8.17",
         settings: {
-          viaIR: true,
-          optimizer: {
-            ...(process.env.NO_SPECIALIZER
-              ? optimizerSettingsNoSpecializer
-              : { enabled: true, runs: 4_294_967_295 }),
-          },
-          metadata: {
-            bytecodeHash: "none",
-          },
-          outputSelection: {
-            "*": {
-              "*": ["evm.assembly", "irOptimized", "devdoc"],
-            },
-          },
-        },
-      },
-    ],
-    overrides: {
-      "contracts/conduit/Conduit.sol": {
-        version: "0.8.14",
-        settings: {
-          viaIR: true,
-          optimizer: {
-            enabled: true,
-            runs: 1000000,
-          },
-        },
-      },
-      "contracts/conduit/ConduitController.sol": {
-        version: "0.8.14",
-        settings: {
-          viaIR: true,
-          optimizer: {
-            enabled: true,
-            runs: 1000000,
-          },
-        },
-      },
-      "contracts/helpers/TransferHelper.sol": {
-        version: "0.8.14",
-        settings: {
-          viaIR: true,
-          optimizer: {
-            enabled: true,
-            runs: 1000000,
-          },
-        },
-      },
-      "order-validator/SeaportValidator.sol": {
-        version: "0.8.17",
-        settings: {
           viaIR: false,
           optimizer: {
             enabled: true,
@@ -129,7 +41,7 @@ const config: HardhatUserConfig = {
           },
         },
       },
-    },
+    ],
   },
   networks: {
     hardhat: {
@@ -141,18 +53,6 @@ const config: HardhatUserConfig = {
         url: process.env.ETH_RPC_URL ?? "",
       },
     },
-    verificationNetwork: {
-      url: process.env.NETWORK_RPC ?? "",
-    },
-  },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
-    outputFile: getReportPathForCommit(),
-    noColors: true,
-  },
-  etherscan: {
-    apiKey: process.env.EXPLORER_API_KEY,
   },
   // specify separate cache for hardhat, since it could possibly conflict with foundry's
   paths: {
