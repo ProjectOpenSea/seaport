25edcd6c928ff6bfb67f8cf83deca4468b0e9757
horsefacts <webmaster@terminally.online>
2023-03-14 13:34:11 -0400

    WIP: generators

---
+++ contracts/helpers/sol/lib/generators/GeneratorUtils.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+uint256 constant UINT256_MAX = type(uint256).max;
+
+function bound(
+    uint256 x,
+    uint256 min,
+    uint256 max
+) pure returns (uint256 result) {
+    require(min <= max, "Max is less than min.");
+    // If x is between min and max, return x directly. This is to ensure that dictionary values
+    // do not get shifted if the min is nonzero. More info: https://github.com/foundry-rs/forge-std/issues/188
+    if (x >= min && x <= max) return x;
+
+    uint256 size = max - min + 1;
+
+    // If the value is 0, 1, 2, 3, warp that to min, min+1, min+2, min+3. Similarly for the UINT256_MAX side.
+    // This helps ensure coverage of the min/max values.
+    if (x <= 3 && size > x) return min + x;
+    if (x >= UINT256_MAX - 3 && size > UINT256_MAX - x)
+        return max - (UINT256_MAX - x);
+
+    // Otherwise, wrap x into the range [min, max], i.e. the range is inclusive.
+    if (x > max) {
+        uint256 diff = x - max;
+        uint256 rem = diff % size;
+        if (rem == 0) return max;
+        result = min + rem - 1;
+    } else if (x < min) {
+        uint256 diff = min - x;
+        uint256 rem = diff % size;
+        if (rem == 0) return min;
+        result = max - rem + 1;
+    }
+}
