a7844a665c3ea81766adff2ce14826b3b3910d52
d1ll0n <dillon@indexed.finance>
2023-03-25 05:17:16 -0500

    handle null caller/recipient

--- test/foundry/new/helpers/FuzzDerivers.sol
+++ test/foundry/new/helpers/FuzzDerivers.sol
@@ -67,15 +67,21 @@ abstract contract FuzzDerivers is
         bytes4 action = context.action();
         Execution[] memory implicitExecutions;
         Execution[] memory explicitExecutions;
+        address caller = context.caller == address(0)
+            ? address(this)
+            : context.caller;
+        address recipient = context.recipient == address(0)
+            ? caller
+            : context.recipient;
         if (
             action == context.seaport.fulfillOrder.selector ||
             action == context.seaport.fulfillAdvancedOrder.selector
         ) {
             implicitExecutions = getStandardExecutions(
                 toOrderDetails(context.orders[0].parameters),
-                context.caller,
+                caller,
                 context.fulfillerConduitKey,
-                context.recipient,
+                recipient,
                 0 // TODO: Native tokens?
             );
         } else if (
@@ -85,7 +91,7 @@ abstract contract FuzzDerivers is
         ) {
             implicitExecutions = getBasicExecutions(
                 toOrderDetails(context.orders[0].parameters),
-                context.caller,
+                caller,
                 context.fulfillerConduitKey,
                 0 // TODO: Native tokens?
             );
@@ -107,8 +113,9 @@ abstract contract FuzzDerivers is
             action == context.seaport.matchAdvancedOrders.selector
         ) {
             FulfillmentComponent[] memory remainingOfferComponents;
+
             (explicitExecutions, implicitExecutions) = getMatchExecutions(
-                toFulfillmentDetails(context.orders, context.recipient),
+                toFulfillmentDetails(context.orders, recipient),
                 context.fulfillments,
                 remainingOfferComponents
             );
