c73a10f4c324bdc72b69aaabe8ea3eae42530a14
0age <0age@protonmail.com>
2023-04-14 10:28:07 -0700

    undo linter change

--- contracts/lib/BasicOrderFulfiller.sol
+++ contracts/lib/BasicOrderFulfiller.sol
@@ -610,9 +610,7 @@ contract BasicOrderFulfiller is OrderValidator {
                     BasicOrder_totalOriginalAdditionalRecipients_cdPtr
                 )
                 let i := 0
-                for {
-
-                } lt(i, totalAdditionalRecipients) {
+                for {} lt(i, totalAdditionalRecipients) {
                     i := add(i, 1)
                 } {
                     /*
@@ -727,9 +725,7 @@ contract BasicOrderFulfiller is OrderValidator {
                     BasicOrder_additionalRecipients_length_cdPtr
                 )
 
-                for {
-
-                } lt(i, totalAdditionalRecipients) {
+                for {} lt(i, totalAdditionalRecipients) {
                     i := add(i, 1)
                 } {
                     // Retrieve calldata pointer for additional recipient.
