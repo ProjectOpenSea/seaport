31bb5cd0e349b0af131fea9f272c7028cb7cc3b6
d1ll0n <dillon@indexed.finance>
2023-04-25 02:17:02 -0500

    Add pure log fn for convenience

--- test/foundry/new/helpers/VmUtils.sol
+++ test/foundry/new/helpers/VmUtils.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.17;
 
 import { Vm } from "forge-std/Vm.sol";
 import { logAssume } from "./Metrics.sol";
+import { console2 } from "forge-std/console2.sol";
 
 address constant VM_ADDRESS = address(
     uint160(uint256(keccak256("hevm cheat code")))
@@ -14,4 +15,17 @@ function assume(bool condition, string memory name) {
         logAssume(name);
     }
     vm.assume(condition);
+}
+
+function _log(string memory str) view {
+    console2.log(str);
+}
+
+function pureLog(string memory str) pure {
+    function(string memory) internal fn1 = _log;
+    function(string memory) internal pure fn2;
+    assembly {
+        fn2 := fn1
+    }
+    fn2(str);
 }
