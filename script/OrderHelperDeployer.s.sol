8a1cb1ee9c6d701f724e6ac4097c76eaecac03bd
horsefacts <webmaster@terminally.online>
2023-06-07 19:56:34 -0400

    add order helper deploy script

---
+++ script/OrderHelperDeployer.s.sol
@@ -0,0 +1,100 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+import "forge-std/Script.sol";
+import "forge-std/console.sol";
+
+import {
+    RequestValidator
+} from "../contracts/helpers/order-helper/lib/RequestValidator.sol";
+import {
+    CriteriaHelper
+} from "../contracts/helpers/order-helper/lib/CriteriaHelper.sol";
+import {
+    ValidatorHelper
+} from "../contracts/helpers/order-helper/lib/ValidatorHelper.sol";
+import {
+    OrderDetailsHelper
+} from "../contracts/helpers/order-helper/lib/OrderDetailsHelper.sol";
+import {
+    FulfillmentsHelper
+} from "../contracts/helpers/order-helper/lib/FulfillmentsHelper.sol";
+import {
+    ExecutionsHelper
+} from "../contracts/helpers/order-helper/lib/ExecutionsHelper.sol";
+import {
+    SeaportOrderHelper
+} from "../contracts/helpers/order-helper/SeaportOrderHelper.sol";
+
+interface ImmutableCreate2Factory {
+    function safeCreate2(
+        bytes32 salt,
+        bytes calldata initializationCode
+    ) external payable returns (address deploymentAddress);
+}
+
+contract OrderHelperDeployer is Script {
+    ImmutableCreate2Factory private constant IMMUTABLE_CREATE2_FACTORY =
+        ImmutableCreate2Factory(0x0000000000FFe8B47B3e2130213B802212439497);
+    address private constant SEAPORT_ADDRESS =
+        0x00000000000000ADc04C56Bf30aC9d3c0aAF14dC;
+    bytes32 private constant SALT = bytes32(uint256(0x1));
+
+    function deploy(
+        string memory name,
+        bytes memory initCode
+    ) internal returns (address) {
+        address deploymentAddress = IMMUTABLE_CREATE2_FACTORY.safeCreate2(
+            SALT,
+            initCode
+        );
+        console.log(name, deploymentAddress);
+        return deploymentAddress;
+    }
+
+    function run() public {
+        vm.startBroadcast();
+
+        address requestValidator = deploy(
+            "requestValidator",
+            type(RequestValidator).creationCode
+        );
+        address criteriaHelper = deploy(
+            "criteriaHelper",
+            type(CriteriaHelper).creationCode
+        );
+        address validatorHelper = deploy(
+            "validatorHelper",
+            type(ValidatorHelper).creationCode
+        );
+        address orderDetailsHelper = deploy(
+            "orderDetailsHelper",
+            type(OrderDetailsHelper).creationCode
+        );
+        address fulfillmentsHelper = deploy(
+            "fulfillmentsHelper",
+            type(FulfillmentsHelper).creationCode
+        );
+        address executionsHelper = deploy(
+            "executionsHelper",
+            type(ExecutionsHelper).creationCode
+        );
+
+        deploy(
+            "orderHelper",
+            bytes.concat(
+                type(SeaportOrderHelper).creationCode,
+                abi.encode(
+                    SEAPORT_ADDRESS,
+                    vm.envAddr("ORDER_VALIDATOR_ADDRESS"),
+                    requestValidator,
+                    criteriaHelper,
+                    validatorHelper,
+                    orderDetailsHelper,
+                    fulfillmentsHelper,
+                    executionsHelper
+                )
+            )
+        );
+    }
+}
